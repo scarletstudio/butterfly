d9cdc4efdc47a8b6ab19ab735a73fc640e85fec6
Vinesh Kannan <vingkan@gmail.com>
2022-04-18 01:13:07 +0000

    Remove unused imports from LoginPage.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 import { signInUser, signOutUser, useCurrentAuthUser } from '../common/utils/auth'
 import { maybeUpdateUserDetails } from '../common/utils/user'
