b763c9465a133697f942d0de7b749c6e370445b8
Vinesh Kannan <vingkan@gmail.com>
2022-05-29 22:05:55 +0000

    Show mock login button only on login page.

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserNinja } from '@fortawesome/free-solid-svg-icons'
+import { Outlet } from 'react-router-dom'
 
 import '../styles/Layout.css'
-
-function MockLoginButton() {
-    return (
-        <div className="FloatingButton MockLoginButton">
-            <Link to="/mocklogin" className="NoDecorate">
-                <FontAwesomeIcon icon={faUserNinja} className="IconBefore" />
-                <span className="FloatingButtonTooltip">Go to Mock Login</span>
-            </Link>
-        </div>
-    )
-}
 
 export default function Layout() {
     return (
         <div className="Layout FullPage ">
             <Outlet />
-            <MockLoginButton />
         </div>
     )
 }
