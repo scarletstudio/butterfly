72be4c3745f12a5a4558635fe1865d8cde461a04
Spriha Jha <jha.spriha@gmail.com>
2022-05-31 15:51:51 +0000

    Added a Profile Page

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import { useCurrentAuthUser } from '../common/utils/auth'
+import { useGetManyUserData } from '../common/utils/user'
+
+export default function ProfilePage() {
+    // Remove authUser once the API is setup
+    useCurrentAuthUser()
+
+    const { uid } = useParams()
+
+    const userProfile = useGetManyUserData({ [uid]: true })?.[uid]
+
+    if (!userProfile) {
+        return <p>User not found.</p>
+    }
+    const { photoURL, displayName } = userProfile
+    return (
+        userProfile && (
+            <div>
+                <div className="ProfileImage">
+                    <img src={photoURL} alt="Profile" className="Image" />
+                </div>
+                <div className="DisplayName">
+                    <span>{displayName}</span>
+                </div>
+            </div>
+        )
+    )
+}
