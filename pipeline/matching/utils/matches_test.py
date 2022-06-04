1e7b589632483ef311cfae7084d758c63c7b6c82
Vinesh Kannan <vingkan@gmail.com>
2022-06-04 19:45:29 +0000

    Add tests for generate_keys().

--- pipeline/matching/utils/matches_test.py
+++ pipeline/matching/utils/matches_test.py
@@ -1,4 +1,9 @@
-from pipeline.matching.utils import get_recently_matched_users_by_user
+import string
+
+from pipeline.matching.utils import (
+    generate_keys,
+    get_recently_matched_users_by_user,
+)
 from pipeline.types import Match
 
 
@@ -17,3 +22,14 @@ def test_get_recently_matched_users_by_user():
         "D": {"A", "C"},
     }
     assert dict(actual) == expected
+
+
+def test_generate_keys():
+    actual = generate_keys(n=10, k=4)
+    valid_chars = set(string.ascii_letters + string.digits)
+    assert len(actual) == 10, "Output contains wrong number of keys."
+    assert len(set(actual)) == len(actual), "Output contains repeated keys."
+    assert all([len(key) == 4 for key in actual]), "Key size is incorrect."
+    assert all(
+        [set(key).issubset(valid_chars) for key in actual]
+    ), "Key contains invalid characters."
