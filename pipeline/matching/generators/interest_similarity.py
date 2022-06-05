5d7e86ddc458c4bf115d229d208564b331bd3308
Vinesh Kannan <vingkan@gmail.com>
2022-06-05 18:08:53 +0000

    Add stubs for interest similarity.

---
+++ pipeline/matching/generators/interest_similarity.py
@@ -0,0 +1,5 @@
+GENERATOR_SIMILAR_INTERESTS = "generateSimilarInterests"
+METADATA_SCORE_SIMILAR_INTERESTS = "score"
+METADATA_COMMON_SIMILAR_INTERESTS = "commonInterests"
+
+# TODO: Implement your generator
