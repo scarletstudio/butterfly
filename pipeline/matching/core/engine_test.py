7a676b4001b8f58a819ff9b08aaa7b9593046962
Vinesh Kannan <vingkan@gmail.com>
2022-06-06 21:45:57 +0000

    Fix bug in matching engine that leaves extra match.

--- pipeline/matching/core/engine_test.py
+++ pipeline/matching/core/engine_test.py
@@ -48,3 +48,50 @@ def test_engine_basic():
         matches=expected_matches,
     )
     assert actual == expected
+
+
+def test_engine_all_priority_matches():
+    # Prepare inputs
+    users = [
+        User(uid="A", displayName="User A"),
+        User(uid="B", displayName="User B"),
+        User(uid="C", displayName="User C"),
+        User(uid="D", displayName="User D"),
+    ]
+    recent_matches = []
+    inp = MatchingInput(
+        community="test",
+        release="2022-04-01",
+        users=users,
+        recent_matches=recent_matches,
+    )
+
+    # Prepare matching engine
+    mock_generator = MagicMock(
+        return_value=[
+            Match(users={"A", "B"}),
+            Match(users={"C", "D"}),
+        ]
+    )
+    mock_ranker = MagicMock(side_effect=lambda x, y: [m for m in y])
+    mock_finalizer = MagicMock(return_value=[])
+    engine = MatchingEngine(
+        generators=[mock_generator],
+        ranker=mock_ranker,
+        finalizer=mock_finalizer,
+    )
+
+    # Run matching engine and verify expected outputs
+    actual = engine.run(inp)
+    expected_matches = [
+        Match(users={"A", "B"}),
+        Match(users={"C", "D"}),
+    ]
+    expected = MatchingOutput(
+        community="test",
+        release="2022-04-01",
+        users=users,
+        matches=expected_matches,
+    )
+    assert actual == expected
+    mock_finalizer.assert_not_called()
