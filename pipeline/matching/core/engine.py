3cc71696484903d478a64b45848268216507d6e3
Vinesh Kannan <vingkan@gmail.com>
2022-06-06 16:50:07 -0500

    Fix bug in matching engine that leaves extra match. (#71)
    * Fix bug in matching engine that leaves extra match.
    
    * Update error message text.

--- pipeline/matching/core/engine.py
+++ pipeline/matching/core/engine.py
@@ -1,6 +1,9 @@
 from typing import Iterator, List, Set, Tuple
 
-from pipeline.matching.core.constants import N_MEMBERS_FOR_FINAL_MATCH
+from pipeline.matching.core.constants import (
+    N_MEMBERS_FOR_FINAL_MATCH,
+    N_MEMBERS_FOR_PRIORITY_MATCH,
+)
 from pipeline.types import (
     Match,
     MatchFinalizer,
@@ -10,6 +13,13 @@ from pipeline.types import (
     MatchRanker,
     UserId,
 )
+
+ERROR_USERS_FOR_PRIORITY_MATCH = "Priority matches should have two users."
+ERROR_USER_NOT_MATCHED = "One user left after selecting priority matches."
+
+
+class UserNotMatchedException(Exception):
+    pass
 
 
 class MatchingEngine:
@@ -23,12 +33,12 @@ class MatchingEngine:
         proposed_matches: Iterator[Match] = self.generate_matches(inp)
         # Rank matches
         ranked_matches: Iterator[Match] = self.ranker(inp, proposed_matches)
-        # Select priority matches
+        # Select priority matches and get remaining users
         results = self.select_priority_matches(inp, ranked_matches)
+        selected: List[Match] = results[0]
+        users: Set[UserId] = results[1]
         # Finalize remaining matches
-        final_matches: List[Match] = self.finalize_remaining_matches(
-            inp, *results
-        )
+        final_matches: List[Match] = self.finalize_matches(inp, selected, users)
         # Return output
         return MatchingOutput(
             community=inp.community,
@@ -51,23 +61,30 @@ class MatchingEngine:
             # If all users have been matched, stop considering matches
             if not users_to_match:
                 break
-            # If remaining users are a group of three, skip tot he final match
+            # If remaining users are a group of three, skip to the final match
             # Note: If only two users remain, continue looking for a match
             if len(users_to_match) == N_MEMBERS_FOR_FINAL_MATCH:
                 break
+            # Logic assumes that priority matches only have two users
+            if len(match.users) != N_MEMBERS_FOR_PRIORITY_MATCH:
+                inp.logger.warning(ERROR_USERS_FOR_PRIORITY_MATCH)
+                continue
             # If all users in this match have yet to be matched, select it
             if match.users.issubset(users_to_match):
                 selected_matches.append(match)
                 users_to_match.difference_update(match.users)
         # Make final match in case there is one group left
         # Note: This time, we consider groups of two or three, not just three
-        if len(users_to_match) <= N_MEMBERS_FOR_FINAL_MATCH:
-            final_match = Match(users=users_to_match)
-            selected_matches.append(final_match)
-            users_to_match = set()
+        if users_to_match:
+            if len(users_to_match) == 1:
+                raise UserNotMatchedException(ERROR_USER_NOT_MATCHED)
+            if len(users_to_match) <= N_MEMBERS_FOR_FINAL_MATCH:
+                final_match = Match(users=users_to_match)
+                selected_matches.append(final_match)
+                users_to_match = set()
         return selected_matches, users_to_match
 
-    def finalize_remaining_matches(
+    def finalize_matches(
         self,
         inp: MatchingInput,
         selected_matches: List[Match],
