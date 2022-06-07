7f3bb32ee497ec7308a16aef702129e29482eb68
Vinesh Kannan <vingkan@gmail.com>
2022-06-07 03:23:33 +0000

    Set up types and extract tasks for ratings.

---
+++ pipeline/extract/ratings_test.py
@@ -0,0 +1,76 @@
+from unittest.mock import MagicMock
+
+from pipeline.extract.ratings import extract_intent_upvotes, extract_match_stars
+from pipeline.types import IntentUpvote, MatchStars
+
+
+def test_extract_match_stars():
+    # Mock database call so that test does not actually query a database
+    raw_ratings = {
+        "1": {
+            "from_user": "A",
+            "value": 3,
+            "match": "aykl0021",
+            "users": ["A", "B"],
+            "generator": "testGenerator",
+        }
+    }
+    mock_get_ratings = MagicMock(return_value=raw_ratings)
+    mock_db = MagicMock()
+    mock_db.reference("ratings/match_stars/test").get = mock_get_ratings
+
+    # Run Prefect task with mock
+    actual: RawUserInterests = extract_match_stars.run(mock_db, "test")
+
+    # Verify that database was queried once with no parameters
+    mock_get_ratings.assert_called_once_with()
+
+    # Verify that output matches expected type and content
+    expected = [
+        MatchStars(
+            from_user="A",
+            value=3,
+            match="aykl0021",
+            users={"A", "B"},
+            generator="testGenerator",
+        )
+    ]
+    assert actual == expected
+
+
+def test_extract_intent_upvotes():
+    # Mock database call so that test does not actually query a database
+    raw_ratings = {
+        "1": {
+            "from_user": "A",
+            "to_user": "B",
+            "value": 1,
+            "intent": "tutoring",
+            "match": "aykl0021",
+            "users": ["A", "B"],
+            "generator": "testGenerator",
+        }
+    }
+    mock_get_ratings = MagicMock(return_value=raw_ratings)
+    mock_db = MagicMock()
+    mock_db.reference("ratings/intent_upvotes/test").get = mock_get_ratings
+
+    # Run Prefect task with mock
+    actual: RawUserInterests = extract_intent_upvotes.run(mock_db, "test")
+
+    # Verify that database was queried once with no parameters
+    mock_get_ratings.assert_called_once_with()
+
+    # Verify that output matches expected type and content
+    expected = [
+        IntentUpvote(
+            from_user="A",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            match="aykl0021",
+            users={"A", "B"},
+            generator="testGenerator",
+        )
+    ]
+    assert actual == expected
---
+++ pipeline/types/rating.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import Literal, Set
+
+from pipeline.types.intent import IntentCode
+from pipeline.types.match import MatchKey
+from pipeline.types.match_metadata import GeneratorId
+from pipeline.types.user import UserId
+
+# Ordinal scale from 1 (worst) to 5 (best), 0 if missing
+StarsValue = Literal[0, 1, 2, 3, 4, 5]
+# Currently an upvote is a single positive value, 0 if missing
+UpvoteValue = Literal[0, 1]
+
+
+@dataclass
+class MatchStars:
+    # ID of the user who gave the rating
+    from_user: UserId
+    # Number of stars given
+    value: StarsValue
+    # Key of the match this rating is for
+    match: MatchKey
+    # Users in the match, including the user who gave the rating
+    users: Set[UserId]
+    # ID of the generator that proposed the match
+    generator: GeneratorId
+
+
+@dataclass
+class IntentUpvote:
+    # ID of the user who gave the upvote
+    from_user: UserId
+    # ID of the user being upvoted
+    to_user: UserId
+    # Value of the upvote
+    value: UpvoteValue
+    # Code for the intent the user is being upvoted for
+    intent: IntentCode
+    # Key of the match this upvote is for
+    match: MatchKey
+    # Users in the match, including the user who gave the upvote
+    users: Set[UserId]
+    # ID of the generator that proposed the match
+    generator: GeneratorId
