8d39db2cbbf6f1e5f53efc34961d50a75f071f1c
Vinesh Kannan <vingkan@gmail.com>
2022-06-13 02:08:27 +0000

    Fix logo image to import asset url.

--- frontend/src/app/ui/Logo.jsx
+++ frontend/src/app/ui/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Logo.css'
+import logoUrl from '../../../public/assets/logo-butterfly-rainbow.png'
 
 export default function Logo({ size, align }) {
     return (
         <div className={`Logo ${size} ${align}`}>
-            <img alt="Butterfly Logo" src="/public/assets/logo-butterfly-rainbow.png" />
+            <img alt="Butterfly Logo" src={logoUrl} />
         </div>
     )
 }
