98b5854edae692eeadf5d4d38e7d0be206f43d12
Vinesh Kannan <vingkan@gmail.com>
2022-06-13 01:52:46 +0000

    Move assets from /src to /public.

--- frontend/src/app/ui/Logo.jsx
+++ frontend/src/app/ui/Logo.jsx
@@ -4,7 +4,7 @@ import './Logo.css'
 export default function Logo({ size, align }) {
     return (
         <div className={`Logo ${size} ${align}`}>
-            <img alt="Butterfly Logo" src="/src/assets/logo-butterfly-rainbow.png" />
+            <img alt="Butterfly Logo" src="/public/assets/logo-butterfly-rainbow.png" />
         </div>
     )
 }
