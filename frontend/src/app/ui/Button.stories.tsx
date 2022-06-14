7fdfb87b497d3d5a06aee4619c8d3953d353a482
Vinesh Kannan <vingkan@gmail.com>
2022-06-14 09:32:07 -0700

    Add icon to core Button component. (#115)
    * Add icon to core Button component.
    
    * Make core Button accessible to keyboard.

--- frontend/src/app/ui/Button.stories.tsx
+++ frontend/src/app/ui/Button.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { faCrown, faGhost } from '@fortawesome/free-solid-svg-icons'
 
 import { Button } from './Button'
 
@@ -12,11 +13,44 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
-    primary: true,
     label: 'Button',
+    primary: true,
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
     label: 'Button',
+}
+
+export const PrimaryWithIconBefore = Template.bind({})
+PrimaryWithIconBefore.args = {
+    label: 'Button',
+    primary: true,
+    iconBefore: faCrown,
+}
+
+export const SecondaryWithIconBefore = Template.bind({})
+SecondaryWithIconBefore.args = {
+    label: 'Button',
+    iconBefore: faGhost,
+}
+
+export const PrimaryWithIconAfter = Template.bind({})
+PrimaryWithIconAfter.args = {
+    label: 'Button',
+    primary: true,
+    iconAfter: faCrown,
+}
+
+export const SecondaryWithIconAfter = Template.bind({})
+SecondaryWithIconAfter.args = {
+    label: 'Button',
+    iconAfter: faGhost,
+}
+
+export const SecondaryWithIconBeforeAndAfter = Template.bind({})
+SecondaryWithIconBeforeAndAfter.args = {
+    label: 'Button',
+    iconBefore: faCrown,
+    iconAfter: faGhost,
 }
--- frontend/src/app/ui/Button.tsx
+++ frontend/src/app/ui/Button.tsx
@@ -1,21 +1,56 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+
 import './Button.css'
 
+import { onEnter } from '../utils'
+
 interface ButtonProps {
-    // Whether or not this is the main action, false if not specified
-    primary?: boolean
     // Button contents
     label: string
-    // Optional on click callback
+    // Whether or not this is the main action, false if not specified
+    primary?: boolean
+    // Optional icon to show to the left of the label
+    iconBefore?: IconDefinition
+    // Optional icon to show to the right of the label
+    iconAfter?: IconDefinition
+    // Optional event handlers
     onClick?: () => void
+    onKeyDown?: () => void
+    // Tells the browser the role of this element
+    role: string
+    // Tells the browser that this element can be focused with the tab key
+    tabIndex: number
 }
 
-export const Button = ({ primary = false, label, ...props }: ButtonProps) => {
+export const Button = ({
+    label,
+    primary = false,
+    iconBefore = null,
+    iconAfter = null,
+    role = 'button',
+    tabIndex = 0,
+    ...props
+}: ButtonProps) => {
     const typeClassName = primary ? 'Primary' : ''
-    const classNames = ['Button', typeClassName].join(' ')
+    const className = ['Button', typeClassName].join(' ')
+    const iconBeforeEl = iconBefore && <FontAwesomeIcon icon={iconBefore} className="IconBefore" />
+    const iconAfterEl = iconAfter && <FontAwesomeIcon icon={iconAfter} className="IconAfter" />
+    const buttonProps = {
+        ...props,
+        className,
+        role,
+        tabIndex,
+        // If the button has an on click handler, make sure it is accessible to
+        // keyboard users via the key down event when they hit enter.
+        onKeyDown: props?.onKeyDown || onEnter(props?.onClick),
+    }
     return (
-        <button type="button" className={classNames} {...props}>
-            {label}
-        </button>
+        <span {...buttonProps}>
+            {iconBeforeEl}
+            <span>{label}</span>
+            {iconAfterEl}
+        </span>
     )
 }
