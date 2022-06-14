fc01793f301c73e17536e42139a42840eaaca0fa
Vinesh Kannan <vingkan@gmail.com>
2022-06-14 06:46:31 +0000

    Move profile page user data request to API.

--- frontend/src/app/utils/Fetch.js
+++ frontend/src/app/utils/Fetch.js
@@ -2,6 +2,19 @@ import { useState, useEffect } from 'react'
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
+/*
+ * Async function to fetch JSON response from the backend API.
+ */
+export async function fetchFromBackend({ route, options = {} }) {
+    const url = `${BACKEND_URL}${route}`
+    const req = await fetch(url, options)
+    const res = await req.json()
+    return res
+}
+
+/*
+ * React hook to fetch JSON response from a given URL.
+ */
 export function useRawFetchJson({ url, options = {}, deps = [] }) {
     const [response, setResponse] = useState(null)
     const [error, setError] = useState(null)
@@ -20,6 +33,9 @@ export function useRawFetchJson({ url, options = {}, deps = [] }) {
     return [response, error]
 }
 
+/*
+ * React hook to fetch JSON response from the backend API.
+ */
 export function useBackendFetchJson({ route, ...args }) {
     return useRawFetchJson({
         url: `${BACKEND_URL}${route}`,
