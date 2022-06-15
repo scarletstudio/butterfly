a4d81e3e08acf549f0a81bd67c0a3530f3e1446e
Vinesh Kannan <vingkan@gmail.com>
2022-06-15 00:06:53 -0700

    Fix public asset import. (#118)

--- frontend/src/app/ui/Logo.jsx
+++ frontend/src/app/ui/Logo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Logo.css'
-import logoUrl from '../../../public/assets/logo-butterfly-rainbow.png'
+// eslint-disable-next-line
+import logoUrl from '/assets/logo-butterfly-rainbow.png'
 
 export default function Logo({ size, align }) {
     return (
