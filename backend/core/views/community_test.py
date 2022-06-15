783c87baf706dbedd1f4e627605db7c58f876736
Vinesh Kannan <vingkan@gmail.com>
2022-06-15 04:08:23 +0000

    Do not update data if user already joined community.

--- backend/core/views/community_test.py
+++ backend/core/views/community_test.py
@@ -19,7 +19,7 @@ def test_join_community_first_community(mock_db):
     actual = c.post(f"/core/community/{community}/join/{uid}")
 
     expected = format_json(message="Successfully joined the community!")
-    # User is not in any other communities, set them active in this one
+    # User is not in any other communities, set them active here.
     expected_post = {"active": True, "joined": SERVER_TIMESTAMP}
     assert_response_match(actual, expected)
     mock_get.assert_called_once_with()
@@ -41,7 +41,7 @@ def test_join_community_already_active_in_other_community(mock_db):
     actual = c.post(f"/core/community/{community}/join/{uid}")
 
     expected = format_json(message="Successfully joined the community!")
-    # User is already active in another community, set them inactive in this one
+    # User is already active in another community, set them inactive here.
     expected_post = {"active": False, "joined": SERVER_TIMESTAMP}
     assert_response_match(actual, expected)
     mock_get.assert_called_once_with()
@@ -66,8 +66,32 @@ def test_join_community_inactive_in_other_communities(mock_db):
     actual = c.post(f"/core/community/{community}/join/{uid}")
 
     expected = format_json(message="Successfully joined the community!")
-    # User is in another community, but inactive, set them active in this one
+    # User is in another community, but inactive, set them active here.
     expected_post = {"active": True, "joined": SERVER_TIMESTAMP}
     assert_response_match(actual, expected)
     mock_get.assert_called_once_with()
     mock_set.assert_called_once_with(expected_post)
+
+
+@with_mock_db
+def test_join_community_already_joined(mock_db):
+    mock_get = MagicMock(
+        return_value={
+            "uid": "spriha",
+            "communities": {"test": {"active": True}},
+        }
+    )
+    mock_db.reference("users/spriha").get = mock_get
+    mock_set = MagicMock()
+    mock_db.reference("users/spriha/communities/test").set = mock_set
+
+    c = Client()
+    community = "test"
+    uid = "spriha"
+    actual = c.post(f"/core/community/{community}/join/{uid}")
+
+    expected = format_json(message="Already joined this community.")
+    assert_response_match(actual, expected)
+    mock_get.assert_called_once_with()
+    # User already joined this community, do not update their data.
+    mock_set.assert_not_called()
