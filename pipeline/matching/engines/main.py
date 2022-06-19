b0d4eb8ae0881ed319a9e6039b22deab878ef1a9
Vinesh Kannan <vingkan@gmail.com>
2022-06-19 06:48:39 +0000

    Add MultiRanker and combine all stubs into main engine.

--- pipeline/matching/engines/main.py
+++ pipeline/matching/engines/main.py
@@ -1,12 +1,42 @@
-from pipeline.matching.core import MatchingEngine
+from pipeline.matching.core import MatchingEngine, MatchRanker
 from pipeline.matching.finalizers import FallbacksFinalizer
-from pipeline.matching.rankers import DefaultRanker
+from pipeline.matching.generators import (
+    CommonLettersGenerator,
+    LimitedSchedulesGenerator,
+    RareIntentsGenerator,
+    RareInterestsGenerator,
+    SimilarIntentsGenerator,
+    SimilarInterestsGenerator,
+    SimilarSchedulesGenerator,
+)
+from pipeline.matching.rankers import (
+    IntentUpvotesRanker,
+    MatchStarsRanker,
+    MultiRanker,
+    QuantityRanker,
+    VarietyRanker,
+)
 
 
 class MainMatchingEngine(MatchingEngine):
     def __init__(self):
         super().__init__(
-            generators=[],
-            ranker=DefaultRanker(),
-            finalizer=FallbacksFinalizer(n_retries=5),
+            generators=[
+                SimilarIntentsGenerator(),
+                SimilarInterestsGenerator(),
+                SimilarSchedulesGenerator(),
+                RareIntentsGenerator(),
+                RareInterestsGenerator(),
+                LimitedSchedulesGenerator(),
+                CommonLettersGenerator(min_common=3),
+            ],
+            ranker=MultiRanker(
+                rankers=[
+                    MatchStarsRanker(),
+                    IntentUpvotesRanker(),
+                    VarietyRanker(),
+                    QuantityRanker(),
+                ]
+            ),
+            finalizer=FallbacksFinalizer(n_retries=10),
         )
