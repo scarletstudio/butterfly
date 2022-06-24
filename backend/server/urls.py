0b29fb46004d32c3aaca02e734a93042b4f7564c
Vinesh Kannan <vingkan@gmail.com>
2022-06-24 06:49:21 +0000

    Migrate core API to Django Ninja.

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,11 +1,17 @@
 from django.urls import include, path
+from ninja import NinjaAPI
+
+from backend.core.urls import router as core_router
 
 from . import views
 
+api = NinjaAPI()
+api.add_router("/core", core_router)
+
 urlpatterns = [
     path("", views.hello),
+    path("", api.urls),
     path("attributes/", include("backend.attributes.urls")),
     path("chat/", include("backend.chat.urls")),
-    path("core/", include("backend.core.urls")),
     path("ratings/", include("backend.ratings.urls")),
 ]
