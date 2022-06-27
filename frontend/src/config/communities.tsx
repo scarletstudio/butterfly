055d272dec1a58af6a70dbd1e8a079ab510a24de
Vinesh Kannan <vingkan@gmail.com>
2022-06-27 13:29:01 -0700

    Add more intents for SDS and demo communities. (#176)

--- frontend/src/config/communities.tsx
+++ frontend/src/config/communities.tsx
@@ -16,8 +16,8 @@ export const COMMUNITY_CONFIG: CommunityConfig = {
         id: 'demo',
         name: 'Demo Community',
         intents: [
-            { code: 'tutoring', name: 'Tutoring' },
-            { code: 'tv-movie-recs', name: 'TV/Movie Recommendations' },
+            { code: 'tutoring-math', name: 'Get tutoring in math' },
+            { code: 'watch-recs', name: 'Get show and movie recommendations' },
         ],
         interests: [
             { code: 'running', name: 'Running' },
@@ -28,7 +28,11 @@ export const COMMUNITY_CONFIG: CommunityConfig = {
     sds: {
         id: 'sds',
         name: 'Scarlet Data Studio',
-        intents: [],
+        intents: [
+            { code: 'tech-careers', name: 'Learn about careers in technology' },
+            { code: 'involve-iit', name: 'Get more involved at Illinois Tech' },
+            { code: 'watch-recs', name: 'Get show and movie recommendations' },
+        ],
         interests: [],
     },
     esi: {
