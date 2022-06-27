08d4a272de7926f60439617fe4507bc01c2e8263
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-06-27 18:46:07 -0500

    Chris_Hunter's first commit (#181)

--- pipeline/transform/user_interests.py
+++ pipeline/transform/user_interests.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import prefect
 from prefect import task
 
 from pipeline.types import RawUserInterests, User
@@ -10,4 +11,8 @@ def augment_users_with_interests(
     users: List[User], raw_interests: RawUserInterests
 ) -> List[User]:
     # TODO: Implement your transform task
+
+    logger = prefect.context.get("logger")
+    logger.info("Chris")
+
     return users
