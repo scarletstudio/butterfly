dc56f856473b348e4a4f40893a60e831fd2ef99e
Chris Hunter <chunter3@hawk.iit.edu>
2022-06-28 21:30:51 +0000

    Removed TODOs

--- pipeline/transform/user_interests.py
+++ pipeline/transform/user_interests.py
@@ -10,8 +10,6 @@ def augment_users_with_interests(
     users: List[User], raw_interests: RawUserInterests
 ) -> List[User]:
 
-    # TODO: Implement your transform task
-
     for user in users:
         interest_lst = []
         for key, value in raw_interests.get(user.uid, {}).items():
