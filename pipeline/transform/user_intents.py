1f62f1a2269864b437feb14abd0b8f026402e138
Sxntaro <azucenah00@gmail.com>
2022-06-28 04:20:29 +0000

    Azucena's first commit.

--- pipeline/transform/user_intents.py
+++ pipeline/transform/user_intents.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import prefect
 from prefect import task
 
 from pipeline.types import RawUserIntents, User
@@ -10,8 +11,6 @@ def augment_users_with_intents(
     users: List[User], raw_intents: RawUserIntents
 ) -> List[User]:
     # TODO: Implement your transform task
-    import prefect
-
     logger = prefect.context.get("logger")
     logger.info("Azu was here!")
     return users
