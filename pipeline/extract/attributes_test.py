f403acb331c73f8cd8bf867b61f03f1c5794694e
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-06-29 11:00:17 -0500

    added additional tests cases for attributes extractions when there's no data. (#219)
    added additional tests for attributes file.

--- pipeline/extract/attributes_test.py
+++ pipeline/extract/attributes_test.py
@@ -52,3 +52,37 @@ def test_extract_intents():
         "3": {"comedy": {"giving": False}},
     }
     assert actual == expected
+
+
+def test_extract_interests_no_data():
+    # Mock database call so that test does not actually query a database
+    mock_get_interests = MagicMock(return_value=None)
+    mock_db = MagicMock()
+    mock_db.reference("attributes/interests/test").get = mock_get_interests
+
+    # Run Prefect task with mock
+    actual: RawUserInterests = extract_interests.run(mock_db, "test")
+
+    # Verify that database was queried once with no parameters
+    mock_get_interests.assert_called_once_with()
+
+    # Verify that output matches expected type and content
+    expected: RawUserInterests = {}
+    assert actual == expected
+
+
+def test_extract_intents_no_data():
+    # Mock database call so that test does not actually query a database
+    mock_get_intents = MagicMock(return_value=None)
+    mock_db = MagicMock()
+    mock_db.reference("attributes/intents/test").get = mock_get_intents
+
+    # Run Prefect task with mock
+    actual: RawUserIntents = extract_intents.run(mock_db, "test")
+
+    # Verify that database was queried once with no parameters
+    mock_get_intents.assert_called_once_with()
+
+    # Verify that output matches expected type and content
+    expected: RawUserIntents = {}
+    assert actual == expected
