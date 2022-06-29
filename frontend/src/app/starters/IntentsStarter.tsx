c067a5f3d7fee2b69da65569dbc8661454a22db7
Liliana Ortiz <89435235+lortiz6@users.noreply.github.com>
2022-06-29 17:17:42 -0500

    Lilana first commit (#218)

--- frontend/src/app/starters/IntentsStarter.tsx
+++ frontend/src/app/starters/IntentsStarter.tsx
@@ -9,7 +9,7 @@ interface IntentsStarterProps {
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
 const IntentsStarterInner = ({}: IntentsStarterProps) => {
-    return <div />
+    return <p>Liliana Ortiz was here!</p>
 }
 
 // TODO: Pass your component its props
