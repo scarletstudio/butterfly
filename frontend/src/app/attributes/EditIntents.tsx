4f046d61de8a652bee0187a814dfa0387ee2f1ae
Marilyn Elaine Flowers <mflowers1@hawk.iit.edu>
2022-07-01 19:39:09 +0000

    Work in Progress on Intents

--- frontend/src/app/attributes/EditIntents.tsx
+++ frontend/src/app/attributes/EditIntents.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './EditIntents.css'
 
 interface EditIntentsProps {
     // TODO: Fill out the props for your component
 }
 
+const Subjects = ({ topic }) => {
+    return <h6>{topic}</h6>
+}
+
+const Checkbox = () => {
+    const [isChecked, setIsChecked] = useState(false)
+    const checkHandler = () => {
+        setIsChecked(!isChecked)
+    }
+
+    return (
+        <div>
+            <input type="checkbox" checked={isChecked} onChange={checkHandler} />
+            <span>I want help on this</span>
+            <br />
+            <p>The checkbox is {isChecked ? 'checked' : 'unchecked'}</p>
+            <input type="checkbox" />
+            <span>I can help others with this</span>
+        </div>
+    )
+}
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
 const EditIntents = ({}: EditIntentsProps) => {
-    return <p> Marilyn Elaine Flowers was here! </p>
+    return (
+        <div>
+            <h2>Intents</h2>
+            <Subjects topic="Learn about Careers in technology" />
+            <Checkbox />
+            <Subjects topic="Get more involved at Illinois Tech" />
+            <Checkbox />
+            <Subjects topic="Get show and movie recommendations" />
+            <Checkbox />
+        </div>
+    )
 }
 
 export default EditIntents
