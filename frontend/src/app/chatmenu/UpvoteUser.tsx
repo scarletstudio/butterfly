c8be5be40caf84647daa9f4b04cc549ed2d7fca5
lacosta2 <lacosta2@hawk.iit.edu>
2022-07-08 22:40:15 +0000

    styling

--- frontend/src/app/chatmenu/UpvoteUser.tsx
+++ frontend/src/app/chatmenu/UpvoteUser.tsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import './UpvoteUser.css'
 import { ChatMenuPageProps } from './ChatMenuPage'
+import { UserData } from '../types'
 
 interface UpvoteUserProps {
     // TODO: Fill out the props for your component
+    participants: Object
 }
 
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
-const UpvoteUserInner = ({}: UpvoteUserProps) => {
+const UpvoteUserInner = (props: UpvoteUserProps) => {
+    const { participants } = props
     return (
         <div>
-            <p>Luis was here part two! </p>
+            <div>
+                <p className="paragraph">
+                    Did your match help you in any of these areas? Give them an upvote so they can
+                    be matched to others looking for support!{' '}
+                </p>
+            </div>
+            <div>
+                {Object.values(participants).map((user: UserData) => (
+                    <div key={user.uid}>
+                        <img src={user.photoURL} alt="" className="profile" />
+                        <p className="username">{user.displayName}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
 
 // TODO: Pass your component its props
 // eslint-disable-next-line no-unused-vars
-const UpvoteUser = ({ chat, config }: ChatMenuPageProps) => <UpvoteUserInner />
+const UpvoteUser = ({ chat, config }: ChatMenuPageProps) => (
+    <UpvoteUserInner participants={chat.participants} />
+)
 
 export default UpvoteUser
