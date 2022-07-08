3381415989d103deb1c8f9aa6d206cb17ea44513
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2022-07-08 15:16:52 +0000

    Created the inital css and added an icon

--- frontend/src/app/chatmenu/SearchMessages.tsx
+++ frontend/src/app/chatmenu/SearchMessages.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import './SearchMessages.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { ChatMenuPageProps } from './ChatMenuPage'
+// import { useGetMessages } from '../chat/ChatHooks'
 
 interface SearchMessagesProps {
     // TODO: Fill out the props for your component
 }
+/*
+const searchbar = document.getElementById("searchbar");
+//const namesFromDOM = document.getElementsBy("useGetMessages");
+searchbar.addEventListener("keyup", (event) => {
+    const {value} = event.target;
+
+    const searchQuery = value.toLowerCase();
+
+    for(const nameElement of useGetMessages()) {
+        let name = nameElement.textContent.toLowerCase();
+
+        if(name.includes(searchQuery)) {
+            nameElement.display = "block";
+        }
+        else {
+            nameElement.display = "none";
+        }
+    }
+});
+*/
 
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
 const SearchMessagesInner = ({}: SearchMessagesProps) => {
-    return <div />
+    return (
+        <div className="Search">
+            <form action="/" method="get">
+                <div className="SearchBox">
+                    <input type="search" placeholder="Search Messages" id="searchbar" />
+                </div>
+                <div className="ButtonSearch">
+                    <button type="button">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 // TODO: Pass your component its props
