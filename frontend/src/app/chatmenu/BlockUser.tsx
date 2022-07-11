cbb5bb63924bc5a32e8f8f7db022a42be9194c25
Spriha Jha <jha.spriha@gmail.com>
2022-07-11 17:29:55 +0000

    Added UI Implementation for BlockUser

--- frontend/src/app/chatmenu/BlockUser.tsx
+++ frontend/src/app/chatmenu/BlockUser.tsx
@@ -1,38 +1,78 @@
 import React from 'react'
 import './BlockUser.css'
-import { ChatMenuPageProps } from './ChatMenuPage'
+import '../ui/User.css'
 
-import { useGetManyUserData } from '../data'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUnlock, faUserLock } from '@fortawesome/free-solid-svg-icons'
+
+import { ChatMenuPageProps } from './ChatMenuPage'
+import { UserData } from '../types/UserData'
+
+// import { useBackendFetchJson } from '../utils'
 
 interface BlockUserProps {
     // TODO: Fill out the props for your component
-    uid: string
-    // blocked:
+    participants: UserData[]
+    // userUid: string
 }
 
-// TODO: Implement your component
-// eslint-disable-next-line no-empty-pattern
-const BlockUserInner = ({ uid }: BlockUserProps) => {
-    const userProfile = useGetManyUserData({ [uid]: true })?.[uid]
-    if (!userProfile) {
-        return <p>User not found.</p>
+const UserTile = ({ user }) => {
+    const { uid, displayName, photoURL } = user
+    const alt = `Profile image for ${displayName}.`
+
+    const doBlock = () => {
+        // TODO: Implement blocking the user method
+        console.log('User to be blocked', uid)
     }
-    // const { photoURL, displayName } = userProfile
-    // uid = // user ID you are checking for
-    // const [res, err] = useBackendFetchJson({
-    // route: `/chat/block/user/${uid}`
-    // })
-    // console.log(res?.blocks)
+
+    const doUnblock = () => {
+        // TODO: Implement unblocking the user method
+    }
+
     return (
-        <p>
-            Grace was here! ${uid} ${userProfile}
-        </p>
+        <div className="UserTile">
+            <div className="ProfileImage">
+                <img src={photoURL} alt={alt} className="Image" />
+            </div>
+            <div className="DisplayName">
+                <span>{displayName}</span>
+            </div>
+            <div>
+                <button type="button">
+                    <FontAwesomeIcon onClick={doBlock} icon={faUserLock} className="IconBefore" />
+                </button>
+                <button type="button">
+                    <FontAwesomeIcon onClick={doUnblock} icon={faUnlock} className="IconBefore" />
+                </button>
+            </div>
+        </div>
     )
 }
 
-// display users to block on chat menu
-// TODO: Pass your component its props
-// eslint-disable-next-line no-unused-vars
-const BlockUser = ({ chat, config }: ChatMenuPageProps) => <BlockUserInner uid={chat?.for} />
+const BlockUserInner = ({ participants }: BlockUserProps) => {
+    console.log(participants)
+    return (
+        <div className="UserRow">
+            {participants.map((user) => (
+                <UserTile key={user?.uid} user={user} />
+            ))}
+        </div>
+    )
+}
+
+const BlockUser = ({ chat }: ChatMenuPageProps) => (
+    <BlockUserInner
+        participants={Object.values(chat?.participants).filter((user) => user?.uid !== chat?.for)}
+        // userUid={chat?.for}
+    />
+)
 
 export default BlockUser
+
+// TODO: (Before Wednesday)
+// 1. Move css to BlockUser.css from User.css for the UserTile
+// 2. Add comments to the different const functionalities for better understanding
+// 3. Implement the onClick methods for block/unblock
+
+// TODO: (After Wednesday)
+// 1. Get blocked users for a user to enable/disable block/unblock buttons
