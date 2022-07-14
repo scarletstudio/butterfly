f4d6ef9e309bda98cd05942078dcb89e1d142f22
BoominHutJr <afahsi@hawk.iit.edu>
2022-07-14 00:26:40 +0000

    second test case draft

--- pipeline/matching/generators/rare_interests.py
+++ pipeline/matching/generators/rare_interests.py
@@ -56,6 +56,7 @@ class RareInterestsGenerator(MatchGenerator):
 
         # comparing the intrests of every possible pair of users
         matches = list(it.combinations(usrList, 2))
+        rareInterests_ = list()
         rarestInterest = " "
         rarityScore = 0.0
         matchNames = set("None")
@@ -66,21 +67,43 @@ class RareInterestsGenerator(MatchGenerator):
             )
 
             for commonInterest in commonInterestList:
-                # rarestInterest = min(rarityScoreDict, key=rarityScoreDict.get)
                 rarestInterest = min(
                     rarityScoreDict.items(), key=lambda x: x[1]
                 )[0]
                 if rarestInterest in commonInterestList:
                     matchNames = {userMatch[0].uid, userMatch[1].uid}
+                    rareInterests_.append(rarestInterest)
                 rarityScore = float(rarityScoreDict[rarestInterest])
+
+                # iterate through usrs to find those with rarest interest.
+                # rarest interest is found above
+        tmp = list()
+        for interest, rarity in rarityScoreDict.items():
+            if rarity == rarityScoreDict[rarestInterest]:
+                tmp.append(interest)
+
+        rareInterests_ = []
+        for usrMatch in matches:
+            for x in tmp:
+                commonInterestList = list(
+                    set([x.name for x in usrMatch[0].interests])
+                    & set([x.name for x in usrMatch[1].interests])
+                )
+                if x in commonInterestList:
+                    matchNames = {usrMatch[0].uid, usrMatch[1].uid}
+                    rareInterests_.append(x)
+                # if (x in [x.name for x in usrMatch[0].interests]) and (x in [x.name for x in usrMatch[1].interests]):
+                #     rareInterests_.append(x)
+                #     matchNames = {usrMatch[0].uid, usrMatch[1].uid}
+                #     print(matchNames)
 
         m = [
             Match(
                 users=matchNames,
                 metadata=MatchMetadata(
                     generator="rareInterestsGenerator",
-                    score=rarityScore,
-                    rareInterests=[rarestInterest],
+                    score=round(rarityScore, 2),
+                    rareInterests=rareInterests_,
                 ),
             )
         ]
--- pipeline/matching/generators/rare_interests_test.py
+++ pipeline/matching/generators/rare_interests_test.py
@@ -11,7 +11,7 @@ from pipeline.types import (
 )
 
 
-def test_example():
+def test_single_rare_interest():
     hiking = Interest(code="hiking", name="Hiking")
     swimming = Interest(code="swimming", name="Swimming")
     reading = Interest(code="reading", name="Reading")
@@ -43,6 +43,49 @@ def test_example():
         ),
     ]
     assert actual == expected
+
+
+def test_multi_rare_intrest():
+    hiking = Interest(code="hiking", name="Hiking")
+    swimming = Interest(code="swimming", name="Swimming")
+    reading = Interest(code="reading", name="Reading")
+    cooking = Interest(code="cooking", name="Cooking")
+
+    inp = MatchingInput(
+        community="test2",
+        release="2022-07-12",
+        users=[
+            User(uid="1", displayName="A", interests=[hiking, swimming]),
+            User(uid="2", displayName="B", interests=[swimming, reading]),
+            User(uid="3", displayName="C", interests=[reading, cooking]),
+            User(uid="4", displayName="D", interests=[swimming, cooking]),
+        ],
+        recent_matches=[],
+        interests=[hiking, swimming, reading, cooking],
+    )
+
+    generator = RareInterestsGenerator(max_frequency=1.0)
+    actual = list(generator.generate(inp))
+
+    expected = [
+        Match(
+            users={"3", "4"},
+            metadata=MatchMetadata(
+                generator="rareInterestsGenerator",
+                score=0.29,
+                rareInterests=["Cooking"],
+            ),
+        ),
+    ]
+    assert actual == expected
+
+
+def test_odd_users():
+    pass
+
+
+def test_null_inputs():
+    pass
 
 
 # TODO: Add more test cases for your logic
