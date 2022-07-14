4f39119d8be26ad3f0143efe77aa6b447c1c7f9d
Vinesh Kannan <vingkan@gmail.com>
2022-07-14 03:06:36 +0000

    Refactor task and tests to display proposed match counts per user.

--- pipeline/load/display_metrics.py
+++ pipeline/load/display_metrics.py
@@ -1,59 +1,34 @@
-import operator
+from typing import Dict, List
 
 import prefect
 from prefect import task
 
-from pipeline.types import MatchingMetrics, MatchingOutput, User
+from pipeline.types import MatchingMetrics, MatchingOutput, User, UserId
+
+
+def render_counts_per_user(
+    users: List[User], counts: Dict[UserId, int]
+) -> List[str]:
+    """Helper method to get lines of counts per user in descending order."""
+    display_names = {user.uid: user.displayName for user in users}
+    # Sort entries by count in descending order
+    sorted_items = sorted(counts.items(), key=lambda p: p[1], reverse=True)
+    lines = []
+    for uid, count in sorted_items:
+        name = display_names.get(uid)
+        lines.append(f"{count} - {name} ({uid})")
+    return lines
 
 
 @task
 def display_internal_matching_metrics(
-    output: MatchingOutput, metric: MatchingMetrics, return_metric=False
+    output: MatchingOutput, metrics: MatchingMetrics
 ):
+    """Task to display matching engine metrics."""
     logger = prefect.context.get("logger")
-    display_names = [user.displayName for user in output.users]
-    unique_names = set()
-    duplicate_names = []
-    for name in display_names:
-        if name in unique_names:
-            duplicate_names.append(name)
-        else:
-            unique_names.add(name)
-    unique_users = [
-        user for user in output.users if user.displayName not in duplicate_names
-    ]
-    alter_metric_output = {
-        uid.replace(
-            uid,
-            next(
-                (user.displayName for user in unique_users if user.uid == uid),
-                "Unknown",
-            ),
-        ): count
-        for uid, count in metric.n_proposed_matches_per_user.items()
-    }
-    if "Unknown" in alter_metric_output:
-        del alter_metric_output["Unknown"]
-    for user in output.users:
-        if user.displayName in duplicate_names:
-            alter_metric_output[
-                f"{user.displayName} (uid: {user.uid})"
-            ] = metric.n_proposed_matches_per_user[user.uid]
-    n_proposed_matches_per_user = dict(
-        sorted(
-            alter_metric_output.items(),
-            key=operator.itemgetter(1),
-            reverse=True,
-        )
+    lines = render_counts_per_user(
+        output.users, metrics.n_proposed_matches_per_user
     )
-    if return_metric:
-        return n_proposed_matches_per_user
-    logger.info("Generating internal matching metrics")
-    logger.info("Displaying number of proposed matches per user")
-    logger.info(
-        "Matches Proposed per User:\n"
-        + "\n".join(
-            f"{count} - {user}"
-            for user, count in n_proposed_matches_per_user.items()
-        )
-    )
+    logger.info("Matches Proposed per User:")
+    for line in lines:
+        logger.info(line)
--- pipeline/load/display_metrics_test.py
+++ pipeline/load/display_metrics_test.py
@@ -1,63 +1,27 @@
 import pytest
 
-from pipeline.load.display_metrics import display_internal_matching_metrics
-from pipeline.types import MatchingMetrics, MatchingOutput, User
+from pipeline.load.display_metrics import render_counts_per_user
+from pipeline.types import User
 
 
-def test_display_logic():
-
-    # This test verifies if the metrics are properly displayed in descending order
-
+def test_render_counts_per_user():
+    # Verifies that the counts are properly displayed in descending order
     users = [
         User(uid="A", displayName="Ayman"),
-        User(uid="B", displayName="Brian"),
+        User(uid="B", displayName="Bridget"),
         User(uid="C", displayName="Chris"),
         User(uid="D", displayName="Dinora"),
         User(uid="E", displayName="Erik"),
     ]
-    metric = MatchingMetrics(
-        n_proposed_matches_per_user={"D": 4, "A": 1, "B": 5, "E": 6, "C": 2}
-    )
-    output = MatchingOutput(community="", release="", users=users, matches=[])
-    actual = display_internal_matching_metrics.run(
-        output, metric, return_metric=True
-    )
-    expected = {"Erik": 6, "Brian": 5, "Dinora": 4, "Chris": 2, "Ayman": 1}
+    counts = {"D": 4, "A": 1, "B": 5, "E": 6, "C": 2}
 
-    print(actual)
+    actual = render_counts_per_user(users, counts)
 
-    assert list(actual.keys()) == list(expected.keys())
-
-    # Since dictionary assertion disregards order, assertion is done using the keys of the actual and expected converted to a list
-    # List assertion takes order into account
-
-
-def test_identical_users():
-
-    # This test verifies how the display function behaves given users w/ identical display names
-
-    users = [
-        User(uid="A", displayName="Ayman"),
-        User(uid="B", displayName="Liliana"),
-        User(uid="C", displayName="Chris"),
-        User(uid="D", displayName="Marilyn"),
-        User(uid="E", displayName="Chris"),
+    expected = [
+        "6 - Erik (E)",
+        "5 - Bridget (B)",
+        "4 - Dinora (D)",
+        "2 - Chris (C)",
+        "1 - Ayman (A)",
     ]
-    metric = MatchingMetrics(
-        n_proposed_matches_per_user={"D": 4, "A": 1, "B": 5, "E": 6, "C": 2}
-    )
-    output = MatchingOutput(community="", release="", users=users, matches=[])
-    actual = display_internal_matching_metrics.run(
-        output, metric, return_metric=True
-    )
-    expected = {
-        "Chris (uid: E)": 6,
-        "Liliana": 5,
-        "Marilyn": 4,
-        "Chris (uid: C)": 2,
-        "Ayman": 1,
-    }
-
-    assert list(actual.keys()) == list(expected.keys())
-
-    # Users w/ identical names will be further distinguished with their uid
+    assert actual == expected
