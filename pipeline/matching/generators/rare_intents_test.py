b92c717d579dc03c02063b99cbc47d7519d10a95
nelmajdoub <nelmajdoub@hawk.iit.edu>
2022-07-15 00:21:21 +0000

    pass first test case

--- pipeline/matching/generators/rare_intents_test.py
+++ pipeline/matching/generators/rare_intents_test.py
@@ -12,7 +12,7 @@ from pipeline.types import (
 )
 
 
-def test_example():
+def testOne():
     tutoring = Intent(code="tutoring", name="Tutoring", side=Side.BLANK)
     seek_tutoring = Intent(code="tutoring", name="Tutoring", side=Side.SEEKING)
     give_tutoring = Intent(code="tutoring", name="Tutoring", side=Side.GIVING)
@@ -61,47 +61,37 @@ def test_example():
 # create some pesudo test cases
 
 
-# def testOne(): #testing if generator works in general
-#     tutoring = Intent(code="tutoring", name="Tutoring", side=Side.BLANK)
-#     seek_tutoring = Intent(code="tutoring", name="Tutoring", side=Side.SEEKING)
-#     give_tutoring = Intent(code="tutoring", name="Tutoring", side=Side.GIVING)
-#     comedy = Intent(code="comedy", name="Comedy", side=Side.BLANK)
-#     seek_comedy = Intent(code="comedy", name="Comedy", side=Side.SEEKING)
-#     give_comedy = Intent(code="comedy", name="Comedy", side=Side.GIVING)
+def testTwo():  # testing if code is pairing up users using code intent without checking sides
+    tutoring = Intent(code="tutoring", name="Tutoring", side=Side.BLANK)
+    seek_tutoring = Intent(code="tutoring", name="Tutoring", side=Side.SEEKING)
+    give_tutoring = Intent(code="tutoring", name="Tutoring", side=Side.GIVING)
+    comedy = Intent(code="comedy", name="Comedy", side=Side.BLANK)
+    seek_comedy = Intent(code="comedy", name="Comedy", side=Side.SEEKING)
+    give_comedy = Intent(code="comedy", name="Comedy", side=Side.GIVING)
 
+    inp = MatchingInput(
+        community="test",
+        release="2022-06-26",
+        users=[
+            User(
+                uid="1", displayName="A", intents=[seek_tutoring, seek_comedy]
+            ),
+            # ------
+            User(uid="2", displayName="B", intents=[give_tutoring]),
+            User(
+                uid="3", displayName="C", intents=[give_tutoring, seek_comedy]
+            ),
+            # ------
+        ],
+        recent_matches=[],
+        intents=[tutoring],
+    )
 
-#     inp = MatchingInput(
-#         community="test",
-#         release="2022-06-26",
-#         users=[
-#             User(
-#                 uid="1", displayName="A", intents=[seek_tutoring, seek_comedy]
-#             ),
-#             #------
-#             User(uid="2", displayName="B", intents=[give_tutoring]),
-#             User(
-#                 uid="3", displayName="C", intents=[give_tutoring, give_comedy]
-#             ),
-#             #------
-#         ],
-#         recent_matches=[],
-#     )
+    generator = RareIntentsGenerator(max_frequency=1 / 2)
+    actual = list(generator.generate(inp))
 
-#     generator = RareIntentsGenerator(max_frequency=1.0)
-#     actual = list(generator.generate(inp))
-
-
-#     expected_intent = IntentMatch(code="comedy", seeker="1", giver="3")
-#     expected = [
-#         Match(
-#             users={"1", "3"},
-#             metadata=MatchMetadata(
-#                 generator="rareIntentsGenerator",
-#                 rareIntents=[expected_intent],
-#             ),
-#         ),
-#     ]
-#     #give_comedy is the rare percentage
+    expected = []
+    assert actual == expected
 
 
 # def testTwo(): # testing if generator works when given no rareness. aka equal number of givers and seekers
