d69fb023f7f96772fdd9018e12eb99e71f810c80
astep2023 <astephens@hawk.iit.edu>
2022-07-15 00:31:42 +0000

    The product is considered done to the 3 points on Issue #85. Considered for edge cases if someone doesn't select a rating or if they try to fully deselect their rating.

--- frontend/src/app/chatmenu/RateMatch.tsx
+++ frontend/src/app/chatmenu/RateMatch.tsx
@@ -4,122 +4,148 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 import './RateMatch.css'
 import { ChatMenuPageProps } from './ChatMenuPage'
 import { fetchFromBackend } from '../utils'
-
+import { useCurrentAuthUser } from '../login'
 
 interface RateMatchProps {
     // TODO: Fill out the props for your component
     user: string
     names: string[]
     users: string[]
-    generator: string
+    generator?: string
     community: string
     match: string
 }
 
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
-const RateMatchInner = (props: RateMatchProps) => {
+const RateMatchInner = ({ generator = 'blank', ...props }: RateMatchProps) => {
     const [rating, setRating] = useState<number>(0)
-    const [style, setStyle] = useState<string>("unselected")
-    function changeRating(num : number){
-        if (rating === num){
-            setRating(0),
-            setStyle("unselected")
+    function changeRating(num: number) {
+        if (rating === num) {
+            setRating(0)
+        } else {
+            setRating(num)
         }
-        else{
-            setRating(num),
-            setStyle("selected")
-        }
-      }
+    }
     // Suppose we have some callback for when you click the submit button:
     async function submitRating() {
-    
-    // Figure out where to get the rating data from
-    const ratingData = {
-      from_user: props.user, //ask about
-      value: rating,
-      community: props.community, //ask about 
-      match: props.match, //ask about
-      users: [
-        props.users
-      ],
-      generator: props.generator,
-      // Fill in the other fields according to the API documentation
-    }
-    const res = await fetchFromBackend(
-        {
-            route: `/ratings/match_stars`,
-            options: 
-                {
+        if (rating === 0){
+            alert("Rating can not be 0")
+        }
+        else{
+            // Figure out where to get the rating data from
+            const ratingData = {
+                from_user: props.user,
+                value: rating,
+                community: props.community,
+                match: props.match,
+                users: [props.users],
+                generator,
+                // Fill in the other fields according to the API documentation
+            }
+            await fetchFromBackend({
+                route: `/ratings/match_stars`,
+                options: {
                     method: 'POST',
                     body: JSON.stringify(ratingData),
-                }
+                },
+            })
+            // "Rating successfully submitted."
         }
-    )
-    console.log(res?.message)
-    // "Rating successfully submitted."
-  }
-  
-  console.log(props.users)
+    }
     return (
         <div>
             <p>
                 How was your match with
                 <span>
                     {' '}
-                    {props.names.map((name, i) => (
-                        <span>
-                            {name}
-                            {props.names.length - 1 === i ? '.' : ','}{' '}
-                        </span>
-                    ))}
+                    {props.names.map((name, i) => {
+                        const index = i
+                        return (
+                            <span key={`${name}-${index}`}>
+                                {name}
+                                {props.names.length - 1 === i ? '.' : ','}{' '}
+                            </span>
+                        )
+                    })}
                 </span>
             </p>
             <p>Submit a rating so we can give you better matches in the future!</p>
             <div>
-                <span
+                <button
+                    className={rating >= 1 ? 'selected' : 'unselected'}
                     onClick={() => changeRating(1)}
-                    className={style}
+                    onKeyDown={() => changeRating(1)}
+                    type="submit"
                 >
-                    <FontAwesomeIcon icon={faStar} />
-                </span>
-                <span
+                    <span className={rating >= 1 ? 'selected' : 'unselected'}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </span>
+                </button>
+
+                <button
+                    className={rating >= 2 ? 'selected' : 'unselected'}
                     onClick={() => changeRating(2)}
-                    className={style}
+                    onKeyDown={() => changeRating(2)}
+                    type="submit"
                 >
-                    <FontAwesomeIcon icon={faStar} />
-                </span>
-                <span
+                    <span className={rating >= 2 ? 'selected' : 'unselected'}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </span>
+                </button>
+                <button
+                    className={rating >= 3 ? 'selected' : 'unselected'}
                     onClick={() => changeRating(3)}
-                    className={style}
+                    onKeyDown={() => changeRating(3)}
+                    type="submit"
                 >
-                    <FontAwesomeIcon icon={faStar} />
-                </span>
-                <span
+                    <span className={rating >= 3 ? 'selected' : 'unselected'}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </span>
+                </button>
+                <button
+                    className={rating >= 4 ? 'selected' : 'unselected'}
                     onClick={() => changeRating(4)}
-                    className={style}
+                    onKeyDown={() => changeRating(4)}
+                    type="submit"
                 >
-                    <FontAwesomeIcon icon={faStar} />
-                </span>
-                <span
+                    <span className={rating >= 4 ? 'selected' : 'unselected'}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </span>
+                </button>
+                <button
+                    className={rating >= 5 ? 'selected' : 'unselected'}
                     onClick={() => changeRating(5)}
-                    className={style}
+                    onKeyDown={() => changeRating(5)}
+                    type="submit"
                 >
-                    <FontAwesomeIcon icon={faStar} />
-                </span>
+                    <span className={rating >= 5 ? 'selected' : 'unselected'}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </span>
+                </button>
             </div>
-            <button onClick={submitRating}>SUBMIT</button>
+            <button type="submit" onClick={submitRating}>
+                SUBMIT
+            </button>
         </div>
     )
 }
 
-
-
 // TODO: Pass your component its props
 // eslint-disable-next-line no-unused-vars
-const RateMatch = ({ chat, config }: ChatMenuPageProps) => (
-    <RateMatchInner user="demo" names={Object.values(chat.participants).map((user) => user.displayName) } users = {Object.values(chat.participants).map((user) => user.uid)} generator = {chat.metadata.generator?.length > 1 ? chat.metadata.generator : ""} community = "demo" match = {chat.id}/>
-    
-)
+const RateMatch = ({ chat }: ChatMenuPageProps) => {
+    const authUser = useCurrentAuthUser()
+    const myUID = authUser?.uid
+    return (
+        <RateMatchInner
+            user={myUID}
+            names={Object.values(chat.participants).map((user) => user.displayName)}
+            users={Object.values(chat.participants).map((user) => user.uid)}
+            generator={chat?.metadata?.generator}
+            community="demo"
+            match={chat.id}
+        />
+    )
+}
 
 export default RateMatch
