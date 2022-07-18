bcdcc8ba280975d4bcc1c5189507f372c93a34b1
lacosta2 <lacosta2@hawk.iit.edu>
2022-07-18 23:54:50 +0000

    intent changes

--- frontend/src/app/chatmenu/UpvoteUser.tsx
+++ frontend/src/app/chatmenu/UpvoteUser.tsx
@@ -1,17 +1,50 @@
 import React from 'react'
 import './UpvoteUser.css'
 import { ChatMenuPageProps } from './ChatMenuPage'
-import { UserData } from '../types'
+import { ChatData, UserData } from '../types'
+import { COMMUNITY_CONFIG } from '../../config/communities'
 
 interface UpvoteUserProps {
     // TODO: Fill out the props for your component
-    participants: Object
+    participants: { [userId: string]: UserData }
+    intents: Array<{
+        code: string
+        giver: string
+        seeker: string
+    }>
 }
+const mockIntent: Array<{
+    code: string
+    giver: string
+    seeker: string
+}> = [
+    {
+        code: 'watch-recs',
+        giver: 'ZnfAz7KaYnQNIUGYqYAFNPhzH373',
+        seeker: '567',
+    },
+    {
+        code: 'tutoring-math',
+        giver: 'NhFa0IpZHxdElyR9PXjhnERB9t43',
+        seeker: '567',
+    },
+]
 
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
 export const UpvoteUserInner = (props: UpvoteUserProps) => {
+    const community = 'demo'
     const { participants } = props
+    const { intents } = props
+    const getIntent = (intentCode): string => {
+        for (let index = 0; index < COMMUNITY_CONFIG[community].intents.length; index += 1) {
+            const intent = COMMUNITY_CONFIG[community].intents[index]
+            if (intent.code === intentCode) {
+                return intent.name
+            }
+        }
+        return 'No intent'
+    }
     return (
         <div>
             <div>
@@ -28,10 +61,14 @@ export const UpvoteUserInner = (props: UpvoteUserProps) => {
                             <p className="username">{user.displayName}</p>
                         </div>
                         <div className="container">
-                            <button className="btn" type="button">
-                                Thumb
-                            </button>
-                            <p className="rec">Get TV recommendations</p>
+                            <button type="button">Up</button>
+                            {Object.values(intents).map((intent) =>
+                                user.uid === intent.giver ? (
+                                    <p className="intent">{getIntent(intent.code)}</p>
+                                ) : (
+                                    <p />
+                                )
+                            )}
                         </div>
                     </div>
                 ))}
@@ -43,7 +80,7 @@ export const UpvoteUserInner = (props: UpvoteUserProps) => {
 // TODO: Pass your component its props
 // eslint-disable-next-line no-unused-vars
 const UpvoteUser = ({ chat, config }: ChatMenuPageProps) => (
-    <UpvoteUserInner participants={chat.participants} />
+    <UpvoteUserInner participants={chat.participants} intents={mockIntent ?? []} />
 )
 
 export default UpvoteUser
