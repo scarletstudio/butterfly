51a332476592b8a93e1a9fd715d5cbbe3e70287c
Marilyn Elaine Flowers <60019181+mflowers1395@users.noreply.github.com>
2022-07-18 15:47:22 -0500

    Adding more intents to the edit profile page. (#283)
    * Adding more intents to the edit profile page.
    
    * Removing extra intents from intents stories file

--- frontend/src/app/attributes/EditIntents.stories.tsx
+++ frontend/src/app/attributes/EditIntents.stories.tsx
@@ -27,10 +27,16 @@ SomeIntentsSelected.args = {
     intents: [
         { code: 'tutoring', name: 'Tutoring' },
         { code: 'tv-movie-recs', name: 'TV/Movie Recommendations' },
+        { code: 'student-org', name: 'Get information on student organizations' },
+        { code: 'commons-food', name: 'Learn more about the commons food' },
+        { code: 'hobbies', name: 'Learn more about different hobbies' },
     ],
     userIntentMap: {
         tutoring: { seeking: true, giving: false },
         'tv-movie-recs': { seeking: true, giving: true },
+        'student-org': { seeking: false, giving: true },
+        'commons-food': { seeking: true, giving: false },
+        hobbies: { seeking: false, giving: false },
     },
     updateIntent: fakeUpdateIntent,
 }
