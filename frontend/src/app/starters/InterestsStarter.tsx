fc9ac1d732d909e8e87dee04be2fce6955e65eee
Liliana Ortiz <lortiz6@hawk.iit.edu>
2022-07-19 23:59:09 +0000

    add space

--- frontend/src/app/starters/InterestsStarter.tsx
+++ frontend/src/app/starters/InterestsStarter.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import './InterestsStarter.css'
 import { ConversationStarterProps } from './ConversationStarter'
+import { pluralize } from '../utils'
 
 interface InterestsStarterProps {
-    // TODO: Fill out the props for your component
+    interestStarter: Array<string>
 }
 
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
-const InterestsStarterInner = ({}: InterestsStarterProps) => {
-    return <div />
+const InterestsStarterInner = ({ interestStarter }: InterestsStarterProps) => {
+    const nInterest = pluralize(interestStarter.length, 'interest')
+    const joinedInterest = interestStarter.join(', ')
+    const anyInterest = `Fun fact, your common interest ${nInterest} are: ${joinedInterest}!`
+    const noInterest = 'Fun fact, you do not share any interest in common!'
+    const message = interestStarter.length > 0 ? anyInterest : noInterest
+    return (
+        <div className="InterestStarter">
+            <p>{message}</p>
+        </div>
+    )
 }
 
 // TODO: Pass your component its props
 // eslint-disable-next-line no-unused-vars
-const InterestsStarter = ({ chat }: ConversationStarterProps) => <InterestsStarterInner />
-
+const InterestsStarter = ({ chat }: ConversationStarterProps) => (
+    <InterestsStarterInner interestStarter={chat?.metadata?.interests || []} />
+)
 export default InterestsStarter
