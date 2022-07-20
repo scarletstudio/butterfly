36f61dd2c2d4c0a6fa9ee16d0c7ba9967bae78be
Liliana Ortiz <lortiz6@hawk.iit.edu>
2022-07-20 00:09:55 +0000

    rm comments

--- frontend/src/app/starters/InterestsStarter.tsx
+++ frontend/src/app/starters/InterestsStarter.tsx
@@ -7,8 +7,6 @@ interface InterestsStarterProps {
     interestStarter: Array<string>
 }
 
-// TODO: Implement your component
-// eslint-disable-next-line no-empty-pattern
 const InterestsStarterInner = ({ interestStarter }: InterestsStarterProps) => {
     const nInterest = pluralize(interestStarter.length, 'interest')
     const joinedInterest = interestStarter.join(', ')
@@ -22,8 +20,6 @@ const InterestsStarterInner = ({ interestStarter }: InterestsStarterProps) => {
     )
 }
 
-// TODO: Pass your component its props
-// eslint-disable-next-line no-unused-vars
 const InterestsStarter = ({ chat }: ConversationStarterProps) => (
     <InterestsStarterInner interestStarter={chat?.metadata?.interests || []} />
 )
