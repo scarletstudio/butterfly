bf6e88ff0fc3c2bda3787e36599533f9fd3e0bb4
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2022-07-21 22:05:16 +0000

    added  some avalibilities test

--- frontend/src/app/starters/SchedulesStarter.stories.tsx
+++ frontend/src/app/starters/SchedulesStarter.stories.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
-import SchedulesStarter from './SchedulesStarter'
+import { SchedulesStarterInner } from './SchedulesStarter'
 
 export default {
     title: 'Conversation Starters/SchedulesStarter',
-    component: SchedulesStarter,
+    component: SchedulesStarterInner,
 }
 
-const Template = (args) => <SchedulesStarter {...args} />
+const Template = (args) => <SchedulesStarterInner {...args} />
 
 // TODO: Set args with the props to test a case of your component
 export const Case1 = Template.bind({})
-Case1.args = {}
+Case1.args = {
+    availabilities: [
+        { day: 'Mon', hour: 9 },
+        { day: 'Tue', hour: 11 },
+        { day: 'Wed', hour: 6 },
+        { day: 'Thu', hour: 13 },
+        { day: 'Fri', hour: 7 },
+    ],
+}
 
 // TODO: Add more cases to test your component
