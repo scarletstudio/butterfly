48da392f8bd5f197c83ec95f204aa26158496f74
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2022-07-21 14:24:12 +0000

    Added cases to SearchMessages stories

--- frontend/src/app/chatmenu/SearchMessages.stories.tsx
+++ frontend/src/app/chatmenu/SearchMessages.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import SearchMessages from './SearchMessages'
+import { getMockUser } from '../mock'
+import { withChatContext } from '../chat/Decorators'
 
 export default {
     title: 'Chat Menu Pages/SearchMessages',
@@ -10,10 +12,34 @@ export default {
 const Template = (args) => <SearchMessages {...args} />
 
 // TODO: Set args with the props to test a case of your component
+const searchmessagesProps = {
+    chat: {
+        for: 'asabe',
+        participants: {
+            asabe: getMockUser('asabe'),
+            pradeep: getMockUser('pradeep'),
+        },
+    },
+    messages: [
+        {
+            from: 'pradeep',
+            type: 'MESSAGE',
+            timestamp: 0,
+            message: "Hey, what's up?",
+        },
+        {
+            from: 'asabe',
+            type: 'MESSAGE',
+            timestamp: 1,
+            message: 'Not much, how about you?',
+        },
+    ],
+}
+
 export const EmptySearch = Template.bind({})
 EmptySearch.args = {}
 
 export const FoundSearch = Template.bind({})
-FoundSearch.args = {}
+FoundSearch.args = [withChatContext({ ...searchmessagesProps })]
 
 // TODO: Add more cases to test your component
