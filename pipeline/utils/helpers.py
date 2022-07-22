9803005d2baada7bf1fb5c44547818d60db2b409
Sxntaro <azucenah00@gmail.com>
2022-07-22 17:04:57 +0000

    Function Update

--- pipeline/utils/helpers.py
+++ pipeline/utils/helpers.py
@@ -4,4 +4,16 @@ from pipeline.types import Match
 
 
 def find_match_of_3(matches: List[Match]) -> Optional[Match]:
-    return None
+    print(matches)
+    for users in matches:
+        if users in matches == 3:
+            return users
+        else:
+            return None
+    return users
+
+    # for users in matches:
+    #     if matches == 3:
+    #         return matches
+    #     else:
+    #         return None
---
+++ pipeline/utils/helpers_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pipeline.types import Match
+
+
+def test_3_match():
+    pass
+
+
+def test_0_match():
+    pass
