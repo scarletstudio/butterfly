50b02e6536f8aa3dc8ae4765ae02f4dc079ab1ea
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-07-24 14:30:59 -0500

    Updated NotificationInfo dataclass; refactored get_users_to_notify fu… (#322)
    * Updated NotificationInfo dataclass; refactored get_users_to_notify function; added Notifier parent class
    
    * Refactored Notification datatypes; modified notification methods and associated tests
    
    * Revised notification datatypes; moved notifer file to pipeline/notifications; revised notification test cases
    
    * Quick change to check_message function

--- pipeline/notifications/check_message_test.py
+++ pipeline/notifications/check_message_test.py
@@ -3,7 +3,15 @@ from datetime import datetime
 import pytest
 
 from pipeline.notifications import NewMessageNotifier
-from pipeline.types import ChatData, Match, Message, User
+from pipeline.types import (
+    ChatData,
+    Match,
+    Message,
+    NotificationInfo,
+    NotificationInput,
+    NotificationType,
+    User,
+)
 
 
 def test_check_msg_notifer():
@@ -13,17 +21,27 @@ def test_check_msg_notifer():
     match = Match(users={"A", "B"})
     a_msg = Message(from_user="A", timestamp=datetime(2022, 7, 16))
     test_participants = {
-        "A": User(uid="A", displayName="Ayman"),
-        "B": User(uid="B", displayName="Brian"),
+        "A": User(uid="A", displayName="Ayman", email="ayman@iit.edu"),
+        "B": User(uid="B", displayName="Brian", email="brian@iit.edu"),
     }
     test_chatdata = ChatData(
         release_timestamp=datetime.now(),
         participants=test_participants,
         messages=[a_msg],
     )
+    test_notif_inp = NotificationInput(
+        chatdata=test_chatdata,
+        run_datetime=datetime.now(),
+    )
     check_notifier = NewMessageNotifier()
-    actual = check_notifier.get_users_to_notify(test_chatdata)
-    assert actual == [User(uid="B", displayName="Brian")]
+    actual = check_notifier.get_notifications(test_notif_inp)
+    assert actual == [
+        NotificationInfo(
+            recipient=User(uid="B", displayName="Brian", email="brian@iit.edu"),
+            notification_type=NotificationType.CHECK_MESSAGE,
+            messagers={"Ayman"},
+        )
+    ]
 
 
 def test_more_users_in_match():
@@ -34,23 +52,41 @@ def test_more_users_in_match():
     b_msg = Message(from_user="B", timestamp=datetime(2022, 7, 17))
     c_msg = Message(from_user="C", timestamp=datetime(2022, 7, 18))
     test_participants = {
-        "A": User(uid="A", displayName="Ayman"),
-        "B": User(uid="B", displayName="Brian"),
-        "C": User(uid="C", displayName="Chris"),
-        "D": User(uid="D", displayName="Dinora"),
-        "E": User(uid="E", displayName="Erik"),
+        "A": User(uid="A", displayName="Ayman", email="ayman@iit.edu"),
+        "B": User(uid="B", displayName="Brian", email="brian@iit.edu"),
+        "C": User(uid="C", displayName="Chris", email="chris@iit.edu"),
+        "D": User(uid="D", displayName="Dinora", email="dinora@iit.edu"),
+        "E": User(uid="E", displayName="Erik", email="erik@iit.edu"),
     }
     test_chatdata = ChatData(
         release_timestamp=datetime.now(),
         participants=test_participants,
         messages=[b_msg, c_msg],
     )
+    test_notif_inp = NotificationInput(
+        chatdata=test_chatdata,
+        run_datetime=datetime.now(),
+    )
     check_notifier = NewMessageNotifier()
-    actual = check_notifier.get_users_to_notify(test_chatdata)
+    actual = check_notifier.get_notifications(test_notif_inp)
     assert actual == [
-        User(uid="A", displayName="Ayman"),
-        User(uid="D", displayName="Dinora"),
-        User(uid="E", displayName="Erik"),
+        NotificationInfo(
+            recipient=User(uid="A", displayName="Ayman", email="ayman@iit.edu"),
+            notification_type=NotificationType.CHECK_MESSAGE,
+            messagers={"Brian", "Chris"},
+        ),
+        NotificationInfo(
+            recipient=User(
+                uid="D", displayName="Dinora", email="dinora@iit.edu"
+            ),
+            notification_type=NotificationType.CHECK_MESSAGE,
+            messagers={"Brian", "Chris"},
+        ),
+        NotificationInfo(
+            recipient=User(uid="E", displayName="Erik", email="erik@iit.edu"),
+            notification_type=NotificationType.CHECK_MESSAGE,
+            messagers={"Brian", "Chris"},
+        ),
     ]
 
 
@@ -62,14 +98,18 @@ def test_no_notification():
     a_msg = Message(from_user="A", timestamp=datetime(2022, 7, 16))
     b_msg = Message(from_user="B", timestamp=datetime(2022, 7, 17))
     test_participants = {
-        "A": User(uid="A", displayName="Ayman"),
-        "B": User(uid="B", displayName="Brian"),
+        "A": User(uid="A", displayName="Ayman", email="ayman@iit.edu"),
+        "B": User(uid="B", displayName="Brian", email="brian@iit.edu"),
     }
     test_chatdata = ChatData(
         release_timestamp=datetime.now(),
         participants=test_participants,
         messages=[a_msg, b_msg],
     )
+    test_notif_inp = NotificationInput(
+        chatdata=test_chatdata,
+        run_datetime=datetime.now(),
+    )
     check_notifier = NewMessageNotifier()
-    actual = check_notifier.get_users_to_notify(test_chatdata)
+    actual = check_notifier.get_notifications(test_notif_inp)
     assert actual == []
