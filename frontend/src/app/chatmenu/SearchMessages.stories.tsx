c65b2648ae4dac7ee892f799fe4a04c1e72d7b18
Nida Akkiswala <77867843+NidaAkkiswala@users.noreply.github.com>
2022-07-25 09:03:45 -0500

    Na updating search messages (#300)
    * Created the inital css and added an icon
    
    * Added Messages prop to SearchMessages component
    
    * Updated MessagesData inteface
    
    * Added implementation for SearchMessages
    
    * Fixed up Search bar and the placement of it.
    
    * Added comments for documentation
    
    * Comments for better documentation
    
    * Showing results addition
    
    * Showing results message
    
    * Template for storybook
    
    * Added cases to SearchMessages stories
    
    * Updated SerchMessages stories to make two cases
    and execute it.
    
    * Removed unnecessay comments
    
    * Removed unneccessary CSS stuff
    
    Co-authored-by: Spriha Jha <jha.spriha@gmail.com>
    Co-authored-by: Spriha Jha <sjha6@hawk.iit.edu>

--- frontend/src/app/chatmenu/SearchMessages.stories.tsx
+++ frontend/src/app/chatmenu/SearchMessages.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import SearchMessages from './SearchMessages'
+import { getMockUser } from '../mock'
 
 export default {
     title: 'Chat Menu Pages/SearchMessages',
@@ -9,8 +10,34 @@ export default {
 
 const Template = (args) => <SearchMessages {...args} />
 
-// TODO: Set args with the props to test a case of your component
-export const Case1 = Template.bind({})
-Case1.args = {}
+const baseChatData = {
+    chat: {
+        for: 'asabe',
+        participants: {
+            asabe: getMockUser('asabe'),
+            pradeep: getMockUser('pradeep'),
+        },
+    },
+    messages: [],
+}
 
-// TODO: Add more cases to test your component
+const shortMessages = [
+    {
+        from: 'pradeep',
+        type: 'MESSAGE',
+        timestamp: 0,
+        message: "Hey, what's up?",
+    },
+    {
+        from: 'asabe',
+        type: 'MESSAGE',
+        timestamp: 1,
+        message: 'Not much, how about you?',
+    },
+]
+
+export const EmptySearch = Template.bind({})
+EmptySearch.args = { ...baseChatData }
+
+export const FoundSearch = Template.bind({})
+FoundSearch.args = { ...baseChatData, messages: [...shortMessages] }
