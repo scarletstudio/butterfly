937c42b9e349829b1d805c401aef82daec404eb0
Vinesh Kannan <vingkan@gmail.com>
2022-07-28 16:10:42 -0500

    Set up analytics collection in the Butterfly app and API. (#346)
    * Add analytics collection endpoint to API.
    
    * Save events in frontend and update backend tests.

---
+++ backend/analytics/views/collect.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict
+
+from ninja import Router, Schema
+
+from backend.utils import SERVER_TIMESTAMP, format_json, get_db
+
+SHOULD_SAVE = False
+
+router = Router()
+
+
+class AnalyticsEvent(Schema):
+    event_type: str
+    data: Dict[str, Any]
+
+
+@router.post("/")
+def post_analytics_data(request, event: AnalyticsEvent):
+    # Add timestamp to event data
+    record = event.dict()
+    record["timestamp"] = SERVER_TIMESTAMP
+
+    # Save event to database
+    db = get_db()
+    ref = db.reference("analytics")
+    # TODO: Remove this check to actually saved the event
+    if SHOULD_SAVE:
+        ref.push(record)
+    return format_json(message="Event saved successfully.")
