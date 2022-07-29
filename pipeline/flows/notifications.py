d0a37c9fc3f8ec41384a647464ab8a6c4318df91
Chris Hunter <chunter3@hawk.iit.edu>
2022-07-29 22:06:18 +0000

    slight progess to notification flow; added prefect task that determines users to notify

--- pipeline/flows/notifications.py
+++ pipeline/flows/notifications.py
@@ -1,19 +1,42 @@
-from typing import Dict
+from typing import Dict, List
 
 import prefect
 from prefect import Flow, Parameter
+from prefect.tasks.secrets import PrefectSecret
+
+from pipeline.extract import extract_users
+from pipeline.notifications import NewMessageNotifier
+from pipeline.transform import convert_users_from_df
+from pipeline.types import NotificationInfo, NotificationInput
+from pipeline.utils.firebase import initialize_firebase_for_prefect
+
+
+@prefect.task
+def get_users_to_notify(inp: NotificationInput) -> List[NotificationInfo]:
+    logger = prefect.context.get("logger")
+    notifier = NewMessageNotifier()
+    logger.info("Determining users to notify")
+    notif_info = notifier.get_notifications(inp=inp)
+    logger.info(f"Done. Number of users to notifiy: {len(notif_info)}")
+    return notif_info
 
 
 def notifications_flow(defaults: Dict = {}) -> Flow:
     with Flow(name="notification_flow") as flow:
-        # Retrieve pipeline parameters
+        # Retrieve community parameter
         param_community = Parameter(
             name="community",
             default=defaults.get("community"),
             required=True,
         )
+        # Configure database connection
+        secret_database_url = PrefectSecret("database_url")
+        secret_admin_credentials = PrefectSecret("admin_credentials")
+        db = initialize_firebase_for_prefect(
+            secret_database_url, secret_admin_credentials
+        )
     """
-     1st Task: Extract chat data for the community in current release
+    Extract chat data for the community in current release
 
      * Will contain messages and information about the match
 
@@ -21,4 +44,8 @@ def notifications_flow(defaults: Dict = {}) -> Flow:
     """
     # TODO: add chat data extraction
 
-    """2nd Task: Extract user in the community"""
+    # Extract user in the community
+    df_users = extract_users(db, param_community)
+
+    # Transform users from dataframe to user objects
+    users_w_profile = convert_users_from_df(df_users)
