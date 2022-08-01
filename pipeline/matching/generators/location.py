8267f04ccc1755cb5abd57c30b7d87f32ecdb00f
verica-kar <vericakaranakova@gmail.com>
2022-08-01 23:51:07 +0000

    commit

--- pipeline/matching/generators/location.py
+++ pipeline/matching/generators/location.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 from typing import Iterator, List
 
 from pipeline.matching.core import MatchGenerator
@@ -8,10 +9,10 @@ GENERATOR_LOCATION = "locationGenerator"
 
 
 class LocationGenerator(MatchGenerator):
-    def __init__(self, min_common: int = 0):
+    def __init__(self):  # , min_common: int = 0):
         # Minimum number of interests users must have in common to be eligible
         # for a similar interests match.
-        self.min_common = min_common  # don't know if I need
+        # self.min_common = min_common  # don't know if I need
         super().__init__(name=GENERATOR_LOCATION)
 
     def generate(self, inp: MatchingInput) -> Iterator[Match]:
@@ -24,7 +25,7 @@ class LocationGenerator(MatchGenerator):
             "McCormick Student Village": "2x2",
             "Rowe Village": "1x1",
             "The Quad": "2x1",
-            "Off-Campus": "10x10",
+            "Off-Campus": "0x0",
         }
         # store same location matches
         matches: List[Match] = []
@@ -43,136 +44,126 @@ class LocationGenerator(MatchGenerator):
             lastUser = inp.users[-1]
             del inp.users[-1]
 
-        # # checks for users that are in the same building
-        # for i in range(len(inp.users)):
-        #     for j in range(i, len(inp.users)):
-        #         if inp.users[i].location == inp.users[j].location:
-        #             matches.append(
-        #                 Match(
-        #                     users={inp.users[i].uid, inp.users[j].uid},
-        #                     metadata=MatchMetadata(
-        #                         generator=GENERATOR_LOCATION,
-        #                         score=0.0,
-        #                         location=[inp.users[i].location],
-        #                     ),
-        #                 )
-        #             )
-        #             # if there is a match delete the second user after creating the match
-        #             del inp.users[j]
+        # checks for users that are in the same building
+        for i in range(len(inp.users)):
+            for j in range(i, len(inp.users)):
+                if inp.users[i].location == inp.users[j].location:
+                    matches.append(
+                        Match(
+                            users={inp.users[i].uid, inp.users[j].uid},
+                            metadata=MatchMetadata(
+                                generator=GENERATOR_LOCATION,
+                                score=0.0,
+                                location=[inp.users[i].location],
+                            ),
+                        )
+                    )
+                    # if there is a match delete the second user after creating the match
+                    del inp.users[j]
 
-        # for i in range(len(inp.users)):
-        #     for j in range(i, len(inp.users)):
-        #         fstULoc = inp.users[i].location
-        #         sndULoc = inp.users[j].location
-        #         dist = sqrt(
-        #             pow(int(fstULoc[0]) - int(sndULoc[0]), 2)
-        #             + pow(int(fstULoc[2:]) - int(sndULoc[2:]), 2)
+        for i in range(len(inp.users)):
+            for j in range(i, len(inp.users)):
+                fstULoc = inp.users[i].location
+                sndULoc = inp.users[j].location
+                dist = math.sqrt(
+                    pow(int(fstULoc[0]) - int(sndULoc[0]), 2)
+                    + pow(int(fstULoc[2:]) - int(sndULoc[2:]), 2)
+                )
+
+                if dist < score:
+                    closestLoc = inp.users[j]
+                    score = dist
+
+                otherMatches.append(
+                    Match(
+                        users={inp.users[i].uid, inp.users[j].uid},
+                        metadata=MatchMetadata(
+                            generator=GENERATOR_LOCATION,
+                            score=score,
+                            location=[fstULoc, sndULoc],
+                        ),
+                    )
+                )
+
+                inp.users.remove(closestLoc)
+
+        matches += otherMatches
+
+        # # different combinations from the list of users
+        # userPairs = list(itertools.combinations(inp.users, r=2))
+
+        # idx = 0
+        # # checking for pairs with the same location and appending them to the list (matches)
+        # for fstUser, sndUser in userPairs:
+        #     if fstUser.location == sndUser.location:
+        #         metadata = MatchMetadata(
+        #             generator=GENERATOR_LOCATION,
+        #             score=1.0,
+        #             location=[fstUser.location, sndUser.location],
+        #         )
+        #         matches.append(
+        #             Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
         #         )
 
-        #         if dist < score:
-        #             closestLoc = inp.users[j]
-        #             score = dist
+        #         # elif (
+        #         #     abs(fstLoc[0] - sndLoc[0]) <= 1
+        #         #     and abs(fstLoc[2:] - sndLoc[2:]) <= 1
+        #         # ):
+        #         #     metadata = MatchMetadata(
+        #         #         generator=GENERATOR_LOCATION,
+        #         #         score=1.0,  # NEED TO FIX
+        #         #         location=[fstUser.location, sndUser.location],
+        #         #     )
+        #         #     matches.append(
+        #         #         Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
+        #         #     )
 
-        #         otherMatches.append(
-        #             Match(
-        #                 users={inp.users[i].uid, inp.users[j].uid},
-        #                 metadata=MatchMetadata(
-        #                     generator=GENERATOR_LOCATION,
-        #                     score=score,
-        #                     location=[fstULoc, sndULoc],
-        #                 ),
-        #             )
+        #         # if the locations match, delete every other pair in userPairs that contains either user
+        #         for i in userPairs:
+        #             if i.__contains__(fstUser) or i.__contains__(sndUser):
+        #                 userPairs.remove(i)
+
+        # #print(matches)
+
+        # # checking for pairs with close locations and appending them to the list (matches)
+        # for fstUser, sndUser in userPairs:
+        #     # location based off of user's dorm building
+        #     fstLoc = dorms[fstUser.location]
+        #     sndLoc = dorms[sndUser.location]
+        #     if (
+        #         abs(int(fstLoc[0]) - int(sndLoc[0])) <= 1
+        #         and abs(int(fstLoc[2:]) - int(sndLoc[2:])) <= 1
+        #     ):
+        #         metadata = MatchMetadata(
+        #             generator=GENERATOR_LOCATION,
+        #             score=1.0,  # NEED TO FIX
+        #             location=[fstUser.location, sndUser.location],
         #         )
+        #         matches.append(
+        #             Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
+        #         )
+        #         # if the locations are close enough, delete every other pair in userPairs that contains either user
+        #         for i in userPairs:
+        #             if i.__contains__(fstUser) or i.__contains__(sndUser):
+        #                 userPairs.remove(i)
 
-        #         inp.users.remove(closestLoc)
+        # #print(matches)
 
-        # matches += otherMatches
-
-        # different combinations from the list of users
-        userPairs = list(itertools.combinations(inp.users, r=2))
-
-        idx = 0
-        # checking for pairs with the same location and appending them to the list (matches)
-        for fstUser, sndUser in userPairs:
-            if fstUser.location == sndUser.location:
-                metadata = MatchMetadata(
-                    generator=GENERATOR_LOCATION,
-                    score=1.0,
-                    location=[fstUser.location, sndUser.location],
-                )
-                matches.append(
-                    Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
-                )
-
-                # elif (
-                #     abs(fstLoc[0] - sndLoc[0]) <= 1
-                #     and abs(fstLoc[2:] - sndLoc[2:]) <= 1
-                # ):
-                #     metadata = MatchMetadata(
-                #         generator=GENERATOR_LOCATION,
-                #         score=1.0,  # NEED TO FIX
-                #         location=[fstUser.location, sndUser.location],
-                #     )
-                #     matches.append(
-                #         Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
-                #     )
-
-                # if the locations match, delete every other pair in userPairs that contains either user
-                for fUser, sUser in userPairs:
-                    if fstUser.uid == fUser.uid or fstUser.uid == sUser.uid:
-                        del userPairs[idx]
-                        idx += 1
-                    if sndUser.uid == fUser.uid or sndUser.uid == sUser.uid:
-                        del userPairs[idx]
-                        idx += 1
-                idx = 0
-
-        # checking for pairs with close locations and appending them to the list (matches)
-        for fstUser, sndUser in userPairs:
-            # location based off of user's dorm building
-            fstLoc = dorms[fstUser.location]
-            sndLoc = dorms[sndUser.location]
-            if (
-                abs(int(fstLoc[0]) - int(sndLoc[0])) <= 1
-                and abs(int(fstLoc[2:]) - int(sndLoc[2:])) <= 1
-            ):
-                metadata = MatchMetadata(
-                    generator=GENERATOR_LOCATION,
-                    score=1.0,  # NEED TO FIX
-                    location=[fstUser.location, sndUser.location],
-                )
-                matches.append(
-                    Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
-                )
-                # if the locations are close enough, delete every other pair in userPairs that contains either user
-                for fUser, sUser in userPairs:
-                    if fstUser.uid == fUser.uid or fstUser.uid == sUser.uid:
-                        del userPairs[idx]
-                        idx += 1
-                    if sndUser.uid == fUser.uid or sndUser.uid == sUser.uid:
-                        del userPairs[idx]
-                        idx += 1
-                idx = 0
-
-        # appending the rest of the matches with far locations to the list (matches)
-        for fstUser, sndUser in userPairs:
-            metadata = MatchMetadata(
-                generator=GENERATOR_LOCATION,
-                score=1.0,  # NEED TO FIX
-                location=[fstUser.location, sndUser.location],
-            )
-            matches.append(
-                Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
-            )
-            # delete every other pair in userPairs that contains either user
-            for fUser, sUser in userPairs:
-                if fstUser.uid == fUser.uid or fstUser.uid == sUser.uid:
-                    del userPairs[idx]
-                    idx += 1
-                if sndUser.uid == fUser.uid or sndUser.uid == sUser.uid:
-                    del userPairs[idx]
-                    idx += 1
-            idx = 0
+        # # appending the rest of the matches with far locations to the list (matches)
+        # for fstUser, sndUser in userPairs:
+        #     metadata = MatchMetadata(
+        #         generator=GENERATOR_LOCATION,
+        #         score=1.0,  # NEED TO FIX
+        #         location=[fstUser.location, sndUser.location],
+        #     )
+        #     matches.append(
+        #         Match(users={fstUser.uid, sndUser.uid}, metadata=metadata)
+        #     )
+        #     # delete every other pair in userPairs that contains either user
+        #     for i in userPairs:
+        #         if i.__contains__(fstUser) or i.__contains__(sndUser):
+        #             userPairs.remove(i)
+        # print(matches)
 
         for m in matches:
             yield m
--- pipeline/matching/generators/location_test.py
+++ pipeline/matching/generators/location_test.py
@@ -1,161 +1,161 @@
-# import pytest
+import pytest
 
-# from pipeline.matching.generators import LocationGenerator
-# from pipeline.types import (
-#     IntentMatch,
-#     Match,
-#     MatchingInput,
-#     MatchMetadata,
-#     Side,
-#     User,
-#     UserId,
-# )
-
-
-# def test_example():
-#     inp = MatchingInput(
-#         community="test",
-#         release="2022-06-26",
-#         users=[
-#             User(
-#                 uid="1",
-#                 displayName="A",
-#                 location="Kacek",
-#             ),
-#             User(uid="2", displayName="B", location="Kacek"),
-#             User(
-#                 uid="3",
-#                 displayName="C",
-#                 location="Kacek",
-#             ),
-#         ],
-#         recent_matches=[],
-#     )
-
-#     generator = LocationGenerator(max_available=1)
-#     actual = list(generator.generate(inp))
-
-#     expected = [
-#         Match(
-#             users={"1", "2", "3"},
-#             metadata=MatchMetadata(
-#                 generator="locationGenerator",
-#                 location=["Kacek", "Kacek", "Kacek"],
-#             ),
-#         )
-#     ]
-
-#     assert actual == expected
+from pipeline.matching.generators.location import LocationGenerator
+from pipeline.types import (
+    IntentMatch,
+    Match,
+    MatchingInput,
+    MatchMetadata,
+    Side,
+    User,
+    UserId,
+)
 
 
-# def test_example2():
-#     inp = MatchingInput(
-#         community="test",
-#         release="2022-06-26",
-#         users=[
-#             User(
-#                 uid="1",
-#                 displayName="A",
-#                 location="Kacek",
-#             ),
-#             User(uid="2", displayName="B", location="Off-Campus"),
-#             User(
-#                 uid="3",
-#                 displayName="C",
-#                 location="Kacek",
-#             ),
-#             User(
-#                 uid="4",
-#                 displayName="D",
-#                 location="Cunningham",
-#             ),
-#         ],
-#         recent_matches=[],
-#     )
+def test_example():
+    inp = MatchingInput(
+        community="test",
+        release="2022-06-26",
+        users=[
+            User(
+                uid="1",
+                displayName="A",
+                location="Kacek",
+            ),
+            User(uid="2", displayName="B", location="Kacek"),
+            User(
+                uid="3",
+                displayName="C",
+                location="Kacek",
+            ),
+        ],
+        recent_matches=[],
+    )
 
-#     generator = LocationGenerator(max_available=1)
-#     actual = list(generator.generate(inp))
+    generator = LocationGenerator()
+    actual = list(generator.generate(inp))
 
-#     expected = [
-#         Match(
-#             users={"1", "3"},
-#             metadata=MatchMetadata(
-#                 generator="locationGenerator",
-#                 location=["Kacek", "Kacek"],
-#             ),
-#         ),
-#         Match(
-#             users={"2", "4"},
-#             metadata=MatchMetadata(
-#                 generator="locationGenerator",
-#                 location=["Off-Campus", "Cunningham"],
-#             ),
-#         ),
-#     ]
+    expected = [
+        Match(
+            users={"1", "2", "3"},
+            metadata=MatchMetadata(
+                generator="locationGenerator",
+                location=["Kacek", "Kacek", "Kacek"],
+            ),
+        )
+    ]
 
-#     assert actual == expected
+    assert actual == expected
 
 
-# def test_example3():
-#     inp = MatchingInput(
-#         community="test",
-#         release="2022-06-26",
-#         users=[
-#             User(
-#                 uid="1",
-#                 displayName="A",
-#                 location="Kacek",
-#             ),
-#             User(uid="2", displayName="B", location="Off-Campus"),
-#             User(
-#                 uid="3",
-#                 displayName="C",
-#                 location="Kacek",
-#             ),
-#             User(
-#                 uid="4",
-#                 displayName="D",
-#                 location="Cunningham",
-#             ),
-#             User(
-#                 uid="5",
-#                 displayName="E",
-#                 location="The Quad",
-#             ),
-#             User(
-#                 uid="6",
-#                 displayName="F",
-#                 location="McCormick Student Village",
-#             ),
-#         ],
-#         recent_matches=[],
-#     )
+def test_example2():
+    inp = MatchingInput(
+        community="test",
+        release="2022-06-26",
+        users=[
+            User(
+                uid="1",
+                displayName="A",
+                location="Kacek",
+            ),
+            User(uid="2", displayName="B", location="Off-Campus"),
+            User(
+                uid="3",
+                displayName="C",
+                location="Kacek",
+            ),
+            User(
+                uid="4",
+                displayName="D",
+                location="Cunningham",
+            ),
+        ],
+        recent_matches=[],
+    )
 
-#     generator = LocationGenerator(max_available=1)
-#     actual = list(generator.generate(inp))
+    generator = LocationGenerator()
+    actual = list(generator.generate(inp))
 
-#     expected = [
-#         Match(
-#             users={"1", "3"},
-#             metadata=MatchMetadata(
-#                 generator="locationGenerator",
-#                 location=["Kacek", "Kacek"],
-#             ),
-#         ),
-#         Match(
-#             users={"4", "6"},
-#             metadata=MatchMetadata(
-#                 generator="locationGenerator",
-#                 location=["Cunningham", "McCormick Student Village"],
-#             ),
-#         ),
-#         Match(
-#             users={"2", "5"},
-#             metadata=MatchMetadata(
-#                 generator="locationGenerator",
-#                 location=["Off-Campus", "The Quad"],
-#             ),
-#         ),
-#     ]
+    expected = [
+        Match(
+            users={"1", "3"},
+            metadata=MatchMetadata(
+                generator="locationGenerator",
+                location=["Kacek", "Kacek"],
+            ),
+        ),
+        Match(
+            users={"2", "4"},
+            metadata=MatchMetadata(
+                generator="locationGenerator",
+                location=["Off-Campus", "Cunningham"],
+            ),
+        ),
+    ]
 
-#     assert actual == expected
+    assert actual == expected
+
+
+def test_example3():
+    inp = MatchingInput(
+        community="test",
+        release="2022-06-26",
+        users=[
+            User(
+                uid="1",
+                displayName="A",
+                location="Kacek",
+            ),
+            User(uid="2", displayName="B", location="Off-Campus"),
+            User(
+                uid="3",
+                displayName="C",
+                location="Kacek",
+            ),
+            User(
+                uid="4",
+                displayName="D",
+                location="Cunningham",
+            ),
+            User(
+                uid="5",
+                displayName="E",
+                location="The Quad",
+            ),
+            User(
+                uid="6",
+                displayName="F",
+                location="McCormick Student Village",
+            ),
+        ],
+        recent_matches=[],
+    )
+
+    generator = LocationGenerator()
+    actual = list(generator.generate(inp))
+
+    expected = [
+        Match(
+            users={"1", "3"},
+            metadata=MatchMetadata(
+                generator="locationGenerator",
+                location=["Kacek", "Kacek"],
+            ),
+        ),
+        Match(
+            users={"4", "6"},
+            metadata=MatchMetadata(
+                generator="locationGenerator",
+                location=["Cunningham", "McCormick Student Village"],
+            ),
+        ),
+        Match(
+            users={"2", "5"},
+            metadata=MatchMetadata(
+                generator="locationGenerator",
+                location=["Off-Campus", "The Quad"],
+            ),
+        ),
+    ]
+
+    assert actual == expected
