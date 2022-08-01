13c93f0d2e4fc8ab3f96d1b1399b12dd6f253a1e
Vinesh Kannan <vingkan@gmail.com>
2022-07-31 21:03:22 -0500

    Add a placeholder for interests starters. (#358)

--- frontend/src/app/starters/InterestsStarter.tsx
+++ frontend/src/app/starters/InterestsStarter.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './InterestsStarter.css'
 import { ConversationStarterProps } from './ConversationStarter'
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface InterestsStarterProps {
     // TODO: Fill out the props for your component
 }
@@ -9,11 +10,11 @@ interface InterestsStarterProps {
 // TODO: Implement your component
 // eslint-disable-next-line no-empty-pattern
 const InterestsStarterInner = ({}: InterestsStarterProps) => {
-    return <div />
+    return <p>You have some interests in common!</p>
 }
 
 // TODO: Pass your component its props
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const InterestsStarter = ({ chat }: ConversationStarterProps) => <InterestsStarterInner />
 
 export default InterestsStarter
