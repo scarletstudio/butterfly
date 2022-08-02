73e21c506b920916112456190d68202462fbda6b
azuhdz7 <49726125+azuhdz7@users.noreply.github.com>
2022-08-02 10:26:21 -0500

    Notifications per Users (#336)
    * Function Setup
    
    * Updated Function
    
    * Finished function and added Test Cases

---
+++ pipeline/utils/notifications_test.py
@@ -0,0 +1,61 @@
+import pytest
+
+from pipeline.testing import equal_lists
+from pipeline.types import NotificationInfo, NotificationType, User
+from pipeline.utils.notifications import notifications_per_user
+
+
+def test_return_lst_1_notification():
+    notifications = [
+        NotificationInfo(
+            recipient=User(uid="A"),
+            notification_type=NotificationType.CHECK_MATCH,
+        )
+    ]
+    actual = notifications_per_user(notifications)
+    expected = [
+        NotificationInfo(
+            recipient=User(uid="A"),
+            notification_type=NotificationType.CHECK_MATCH,
+        )
+    ]
+
+    assert actual == expected
+
+
+def test_return_lst_with_double_notifications():
+    notifications = [
+        NotificationInfo(
+            recipient=User(uid="A"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+        NotificationInfo(
+            recipient=User(uid="B"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+        NotificationInfo(
+            recipient=User(uid="C"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+        NotificationInfo(
+            recipient=User(uid="B"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+    ]
+    actual = notifications_per_user(notifications)
+    expected = [
+        NotificationInfo(
+            recipient=User(uid="A"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+        NotificationInfo(
+            recipient=User(uid="B"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+        NotificationInfo(
+            recipient=User(uid="C"),
+            notification_type=NotificationType.CHECK_MATCH,
+        ),
+    ]
+
+    assert equal_lists(actual, expected)
