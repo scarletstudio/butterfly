732855b12c12337adbbeb8af4a8293f54ca555d6
verica-kar <77712531+verica-kar@users.noreply.github.com>
2022-08-05 17:40:31 -0500

    Vk branch (#342)
    * first changes
    
    * Current Code
    
    * Update intent_upvotes.py
    
    * Update intent_upvotes_test.py
    
    * working commit
    
    * Update intent_upvotes_test.py
    
    * works
    
    * Core Project
    
    * Core Project
    
    * VK Core Project
    
    Co-authored-by: Anna L Benson <annalucybenson@gmail.com>

--- pipeline/matching/rankers/intent_upvotes.py
+++ pipeline/matching/rankers/intent_upvotes.py
@@ -1,7 +1,14 @@
-from typing import Iterator
+from typing import Dict, Iterator, List, Tuple
 
 from pipeline.matching.core import MatchRanker
-from pipeline.types import Match, MatchingInput
+from pipeline.types import (
+    IntentMatch,
+    IntentUpvote,
+    Match,
+    MatchingInput,
+    MatchMetadata,
+    Side,
+)
 
 RANKER_INTENT_UPVOTES = "intentUpvotesRanker"
 
@@ -14,5 +21,62 @@ class IntentUpvotesRanker(MatchRanker):
         self, inp: MatchingInput, proposed: Iterator[Match]
     ) -> Iterator[Match]:
         # TODO: Implement your ranker
-        inp.logger.info("Verica Karanakokva was here!")
-        yield from proposed
+        # rank halping variables
+        flag = False
+        idx = 0
+        upvotedDict: Dict[Tuple[int, str], int] = {}
+        upvotedLst: List[Match] = []
+        lst: List[Match] = []
+
+        # match specific variables
+        currMatch = next(proposed)
+        ups = inp.intent_upvotes
+        failMatch = Match(
+            users={"fail", "fail"},
+            metadata=MatchMetadata(
+                generator="fail",
+                matchingIntents=[
+                    IntentMatch(code="failing", seeker="fail", giver="fail")
+                ],
+            ),
+        )
+
+        while currMatch != failMatch:
+            if currMatch.metadata.matchingIntents != []:
+                currMatchGiver = currMatch.metadata.matchingIntents[0].giver
+                currMatchIntent = currMatch.metadata.matchingIntents[0].code
+                for i in ups:
+                    if (
+                        i.to_user == currMatchGiver
+                        and i.intent == currMatchIntent
+                    ):
+                        flag = True
+                        if upvotedLst.__contains__(currMatch):
+                            upvotedDict[
+                                (upvotedLst.index(currMatch), currMatchIntent)
+                            ] += i.value
+                        else:
+                            upvotedLst.append(currMatch)
+                            upvotedDict[
+                                (len(upvotedLst) - 1, currMatchIntent)
+                            ] = i.value
+
+                if flag == False:
+                    lst.append(currMatch)
+            else:
+                lst.append(currMatch)
+            try:
+                currMatch = next(proposed)
+            except:
+                currMatch = failMatch
+
+        flag = False
+
+        while upvotedDict != {}:
+            maxKey = max(upvotedDict, key=lambda k: upvotedDict[k])
+            lst.insert(idx, upvotedLst[maxKey[0]])
+            idx += 1
+            del upvotedDict[maxKey]
+
+        for l in lst:
+            yield l
--- pipeline/matching/rankers/intent_upvotes_test.py
+++ pipeline/matching/rankers/intent_upvotes_test.py
@@ -54,7 +54,7 @@ def test_example():
     ]
 
     ranker = IntentUpvotesRanker()
-    actual = list(ranker.rank(inp, proposed))
+    actual = list(ranker.rank(inp, iter(proposed)))
 
     expected = [
         Match(
@@ -77,8 +77,359 @@ def test_example():
         ),
     ]
     # TODO: Uncomment this assertion and delete the proposed assert
-    # assert actual == expected
-    assert actual == proposed
+    assert actual == expected
+    # assert actual == proposed
 
 
 # TODO: Add more test cases for your logic
+def test_example2():
+    intent_upvotes = [
+        IntentUpvote(
+            from_user="D",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"D", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+    ]
+    inp = MatchingInput(
+        community="test",
+        release="2022-04-01",
+        users=[],
+        recent_matches=[],
+        intent_upvotes=intent_upvotes,
+    )
+    proposed = [
+        Match(
+            users={"C", "D"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="C", giver="D")
+                ],
+            ),
+        ),
+        Match(
+            users={"E", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="E", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"A", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="B")
+                ],
+            ),
+        ),
+    ]
+
+    ranker = IntentUpvotesRanker()
+    actual = list(ranker.rank(inp, iter(proposed)))
+
+    expected = [
+        Match(
+            users={"E", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="E", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"A", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="B")
+                ],
+            ),
+        ),
+        Match(
+            users={"C", "D"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="C", giver="D")
+                ],
+            ),
+        ),
+    ]
+    assert actual == expected
+
+
+def test_example3():
+    intent_upvotes = [
+        IntentUpvote(
+            from_user="D",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"D", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="D",
+            to_user="C",
+            value=1,
+            intent="studying",
+            community="test",
+            match="1",
+            users={"D", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="C",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"C", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="studying",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+    ]
+    inp = MatchingInput(
+        community="test",
+        release="2022-04-01",
+        users=[],
+        recent_matches=[],
+        intent_upvotes=intent_upvotes,
+    )
+    proposed = [
+        Match(
+            users={"A", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"D", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="D", giver="B")
+                ],
+            ),
+        ),
+    ]
+
+    ranker = IntentUpvotesRanker()
+    actual = list(ranker.rank(inp, iter(proposed)))
+
+    expected = [
+        Match(
+            users={"D", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="D", giver="B")
+                ],
+            ),
+        ),
+        Match(
+            users={"A", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="C")
+                ],
+            ),
+        ),
+    ]
+
+    assert actual == expected
+
+
+def test_example4():
+    intent_upvotes = [
+        IntentUpvote(
+            from_user="D",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"D", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="D",
+            to_user="C",
+            value=1,
+            intent="studying",
+            community="test",
+            match="1",
+            users={"D", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="C",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"C", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="studying",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+    ]
+    inp = MatchingInput(
+        community="test",
+        release="2022-04-01",
+        users=[],
+        recent_matches=[],
+        intent_upvotes=intent_upvotes,
+    )
+    proposed = [
+        Match(
+            users={"A", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"E", "H"},
+            metadata=MatchMetadata(generator="someOtherGenerator"),
+        ),
+        Match(
+            users={"D", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="D", giver="B")
+                ],
+            ),
+        ),
+    ]
+
+    ranker = IntentUpvotesRanker()
+    actual = list(ranker.rank(inp, iter(proposed)))
+
+    expected = [
+        Match(
+            users={"D", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="D", giver="B")
+                ],
+            ),
+        ),
+        Match(
+            users={"A", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"E", "H"},
+            metadata=MatchMetadata(generator="someOtherGenerator"),
+        ),
+    ]
+
+    assert actual == expected
