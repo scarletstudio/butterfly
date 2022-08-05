36a226359ffd156c24656b0b768432a441314e2b
Anna L Benson <annalucybenson@gmail.com>
2022-08-05 22:53:08 +0000

    checking out main version of intent upvote tests

--- pipeline/matching/rankers/intent_upvotes_test.py
+++ pipeline/matching/rankers/intent_upvotes_test.py
@@ -301,3 +301,135 @@ def test_example3():
     ]
 
     assert actual == expected
+
+
+def test_example4():
+    intent_upvotes = [
+        IntentUpvote(
+            from_user="D",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"D", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="D",
+            to_user="C",
+            value=1,
+            intent="studying",
+            community="test",
+            match="1",
+            users={"D", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"A", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="C",
+            to_user="B",
+            value=1,
+            intent="tutoring",
+            community="test",
+            match="1",
+            users={"C", "B"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+        IntentUpvote(
+            from_user="A",
+            to_user="C",
+            value=1,
+            intent="studying",
+            community="test",
+            match="1",
+            users={"A", "C"},
+            generator="blueGenerator",
+            timestamp=0,
+        ),
+    ]
+    inp = MatchingInput(
+        community="test",
+        release="2022-04-01",
+        users=[],
+        recent_matches=[],
+        intent_upvotes=intent_upvotes,
+    )
+    proposed = [
+        Match(
+            users={"A", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"E", "H"},
+            metadata=MatchMetadata(generator="someOtherGenerator"),
+        ),
+        Match(
+            users={"D", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="D", giver="B")
+                ],
+            ),
+        ),
+    ]
+
+    ranker = IntentUpvotesRanker()
+    actual = list(ranker.rank(inp, iter(proposed)))
+
+    expected = [
+        Match(
+            users={"D", "B"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="D", giver="B")
+                ],
+            ),
+        ),
+        Match(
+            users={"A", "C"},
+            metadata=MatchMetadata(
+                generator="similarIntentGenerator",
+                matchingIntents=[
+                    IntentMatch(code="tutoring", seeker="A", giver="C")
+                ],
+            ),
+        ),
+        Match(
+            users={"E", "H"},
+            metadata=MatchMetadata(generator="someOtherGenerator"),
+        ),
+    ]
+
+    assert actual == expected
