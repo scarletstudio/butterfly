e65c2c9d9eb7f9f0788ace0cda5bf0d4fe802347
Bridget Torres <bridget.torres2418@gmail.com>
2022-08-06 00:57:00 +0000

    fixed issues

--- pipeline/notifications/notifier.py
+++ pipeline/notifications/notifier.py
@@ -1,5 +1,9 @@
 from pipeline.types import NotificationInfo, NotificationInput
 
+"""This class only considers one chat at a time
+The input type contains data about the chat, as well as the current date/time
+The output type is a list, because there should be more than one person receiving a notification"""
+
 
 class Notifier:
 
@@ -14,9 +18,3 @@ class Notifier:
     def __init__(self, name: str, min_chat: int = 0):
         self.name = name
         self.min_chat = min_chat
-
-
-"""This class only considers one chat at a time
-The input type contains data about the chat, as well as the current date/time
-The output type is a list, because there should be more than one person receiving a notification"""
-# When the week is almost over, if the user chatted with their match, then notify them to rate their match
