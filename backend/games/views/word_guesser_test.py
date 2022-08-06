1f76991682041b37bbddb665bdb583cfac168acf
Vinesh Kannan <vingkan@gmail.com>
2022-08-06 00:47:24 +0000

    Add tests for restart endpoint.

--- backend/games/views/word_guesser_test.py
+++ backend/games/views/word_guesser_test.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from django.test import Client
 
@@ -44,3 +44,35 @@ def test_submit_many_guesses(mock_db):
     # TODO: The goal word is not deterministic, so we are not able to verify
     # the results data, but we can verify that the database sets are called.
     assert len(mock_set.mock_calls) == 2
+
+
+@with_mock_db
+@patch("backend.games.views.word_guesser.games_by_id", {"test": True})
+def test_restart_game(mock_db):
+    mock_delete = MagicMock()
+    mock_db.reference("games/wordguesser/test").delete = mock_delete
+
+    c = Client()
+    game_id = "test"
+    url = f"/games/wordguesser/{game_id}/restart"
+    actual = c.post(url)
+
+    expected = format_json(status=200, message="Restarted game.")
+    assert_response_match(actual, expected)
+    mock_delete.assert_called()
+
+
+@with_mock_db
+@patch("backend.games.views.word_guesser.games_by_id", {})
+def test_restart_game_invalid_id(mock_db):
+    mock_delete = MagicMock()
+    mock_db.reference("games/wordguesser/test").delete = mock_delete
+
+    c = Client()
+    game_id = "test"
+    url = f"/games/wordguesser/{game_id}/restart"
+    actual = c.post(url)
+
+    expected = format_json(status=200, message="No game started for ID: test")
+    assert_response_match(actual, expected)
+    mock_delete.assert_not_called()
