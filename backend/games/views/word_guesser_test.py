39c0ae02e438dc6d1d55931cb8af6b9fdfef3b2b
Vinesh Kannan <vingkan@gmail.com>
2022-08-06 00:40:36 +0000

    Update database mock tests.

--- backend/games/views/word_guesser_test.py
+++ backend/games/views/word_guesser_test.py
@@ -8,9 +8,8 @@ from backend.utils import format_json
 
 @with_mock_db
 def test_submit_one_guess(mock_db):
-    # TODO: Validate mock database calls
-    mock_update = MagicMock()
-    mock_db.reference("games/wordguesser/test").update = mock_update
+    mock_set = MagicMock()
+    mock_db.reference("games/wordguesser/test").set = mock_set
 
     c = Client()
     game_id = "test"
@@ -20,14 +19,16 @@ def test_submit_one_guess(mock_db):
 
     expected = format_json(status=200, message="Guess submitted successfully.")
     assert_response_match(actual, expected)
-    mock_update.assert_not_called()
+
+    # TODO: The goal word is not deterministic, so we are not able to verify
+    # the results data, but we can verify that the database set is called.
+    mock_set.assert_called()
 
 
 @with_mock_db
 def test_submit_many_guesses(mock_db):
-    # TODO: Validate mock database calls
-    mock_update = MagicMock()
-    mock_db.reference("games/wordguesser/test").update = mock_update
+    mock_set = MagicMock()
+    mock_db.reference("games/wordguesser/test").set = mock_set
 
     c = Client()
     game_id = "test"
@@ -39,4 +40,7 @@ def test_submit_many_guesses(mock_db):
     expected = format_json(status=200, message="Guess submitted successfully.")
     assert_response_match(actual1, expected)
     assert_response_match(actual2, expected)
-    mock_update.assert_not_called()
+
+    # TODO: The goal word is not deterministic, so we are not able to verify
+    # the results data, but we can verify that the database sets are called.
+    assert len(mock_set.mock_calls) == 2
