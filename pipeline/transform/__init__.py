df16140ff4ef5115527682b2d0e8cff01325bd54
lwilliams717 <82055308+lwilliams717@users.noreply.github.com>
2022-08-08 22:02:42 -0500

    Response rate analytics  (#388)
    * flow analytics added
    
    * chat response analytics

--- pipeline/transform/__init__.py
+++ pipeline/transform/__init__.py
@@ -1,3 +1,5 @@
+from pipeline.transform.analytics import *
+from pipeline.transform.chat_analytics import *
 from pipeline.transform.compute_metrics import *
 from pipeline.transform.matches import *
 from pipeline.transform.user_intents import *
