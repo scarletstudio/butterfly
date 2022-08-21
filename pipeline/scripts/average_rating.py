57b57cf925de1b672b0670390849ef7033fad28d
astep2023 <86892826+astep2023@users.noreply.github.com>
2022-08-20 20:26:36 -0400

    Completed average_rating script (#362)
    * Completed average_rating script
    
    * Unit testing
    
    * Fix import issues.
    
    Co-authored-by: Vinesh Kannan <vingkan@gmail.com>

---
+++ pipeline/scripts/average_rating.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.path.append("./")
+
+
+def calculate_analytics(matches):
+    totals = dict()
+    count = dict()
+    averages = dict()
+    for match in matches:
+        totals[match["from_user"]] = match["value"] + totals.get(
+            match["from_user"], 0
+        )
+        count[match["from_user"]] = 1 + count.get(match["from_user"], 0)
+    keys = list(totals.keys())
+    for i in range(len(totals)):
+        averages[keys[i]] = totals[keys[i]] / count[keys[i]]
+    return averages
+
+
+def average_rating():
+    from backend.utils.firebase import get_db, initialize_firebase_for_api
+
+    initialize_firebase_for_api()
+    if len(sys.argv) > 1:
+        community = sys.argv[1]
+    else:
+        community = "sds"
+    db = get_db()
+    ref = db.reference(f"ratings/match_stars/{community}/")
+    ratings = ref.get()
+    matches = list(ratings.values())
+    calculate_analytics(matches)
+
+
+if __name__ == "__main__":
+    average_rating()
